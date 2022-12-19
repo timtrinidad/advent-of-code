22d0c4b541fbb3a235525a5341ed87d434841d01
Tim Trinidad <timtrinidad@gmail.com>
2022-12-18 21:40:17 -0800

    feat(2022-18): add challenge part 2 - 1.89s

--- lib/2022/17.ex
+++ lib/2022/17.ex
@@ -13,59 +13,66 @@ aoc 2022, 17 do
     moves = input |> String.trim() |> String.graphemes()
     IO.inspect(moves |> length, label: "Move list length")
     locations = make_move(MapSet.new(), generate_rock(0, 3), [0, 1, 2, 3, 4], moves, num_moves)
-    #    render(locations)
+    # render(locations)
     locations |> locations_max_y |> Kernel.+(1) |> IO.inspect(label: "Max Y")
   end
 
   @doc "Recursive function to simulate rock movement."
   def make_move(locations, curr_rock, next_rock_types, [curr_move | next_moves], num_rocks) do
     # Progress
-    if rem(num_rocks, 200) == 0, do: IO.puts("# rocks remaining: #{num_rocks}")
+    #    if rem(num_rocks, 200) == 0, do: IO.puts("# rocks remaining: #{num_rocks}")
 
-    # Base case - no more rocks to drop
-    if num_rocks == 0 do
-      locations
-    else
-      # Move the rock to the side if possible - coordinates don't change if it's an impossible move
-      {_, curr_rock} = move_rock(locations, curr_rock, curr_move)
-      # Move rock down if possible
-      {valid_y_move, curr_rock} = move_rock(locations, curr_rock, "v")
+    # Move the rock to the side if possible - coordinates don't change if it's an impossible move
+    {_, curr_rock} = move_rock(locations, curr_rock, curr_move)
+    # Move rock down if possible
+    {valid_y_move, curr_rock} = move_rock(locations, curr_rock, "v")
 
-      # If could not move down, lock current rock coordinates into locations set
-      locations =
-        if valid_y_move == :ok,
-          do: locations,
-          else: MapSet.union(locations, MapSet.new(curr_rock))
+    cond do
+      # Base case
+      num_rocks == 0 ->
+        locations
 
-      locations_max_y = locations_max_y(locations)
+      # Valid move - put current move at end of list and move again
+      valid_y_move == :ok ->
+        make_move(locations, curr_rock, next_rock_types, next_moves ++ [curr_move], num_rocks)
 
-      # Move current rock type (head) to the end of the list
-      next_rock_types =
-        if valid_y_move == :ok,
-          do: next_rock_types,
-          else: tl(next_rock_types) ++ [hd(next_rock_types)]
+      # Put current rock in permanent location
+      true ->
+        locations = curr_rock |> Enum.reduce(locations, &MapSet.put(&2, &1))
+        max_y = locations_max_y(locations)
 
-      # Decrement the num_rocks counter if we've moved onto a new rock
-      num_rocks = if valid_y_move == :ok, do: num_rocks, else: num_rocks - 1
+        [curr_type | next_types] = next_rock_types
+        # IO.puts("#{curr_type}, #{curr_rock |> Enum.min_by(&elem(&1, 0)) |> elem(0)}")
 
-      # If moving onto a new rock, generate a new rock in the span position
-      curr_rock =
-        if valid_y_move == :ok,
-          do: curr_rock,
-          else: generate_rock(hd(next_rock_types), locations_max_y + 4)
+        # Move curr rock type to the end
+        next_rock_types = next_types ++ [curr_type]
 
-      # Recursively move rock
-      make_move(locations, curr_rock, next_rock_types, next_moves ++ [curr_move], num_rocks)
+        make_move(
+          locations,
+          # Make new rock based on next type 4 spots above current max
+          generate_rock(hd(next_rock_types), max_y + 4),
+          # Move next rock type to end of list
+          next_rock_types,
+          # Move current move to end of list
+          next_moves ++ [curr_move],
+          # decrement remaining rock count
+          num_rocks - 1
+        )
     end
   end
 
   @doc "Generate a rock with the bottom at the given base_y"
   def generate_rock(rock_type, base_y) do
     case rock_type do
+      # 一
       0 -> [{2, base_y}, {3, base_y}, {4, base_y}, {5, base_y}]
+      # 十
       1 -> [{3, base_y}, {2, base_y + 1}, {3, base_y + 1}, {4, base_y + 1}, {3, base_y + 2}]
+      # 𠃎
       2 -> [{2, base_y}, {3, base_y}, {4, base_y}, {4, base_y + 1}, {4, base_y + 2}]
+      # 丨
       3 -> [{2, base_y}, {2, base_y + 1}, {2, base_y + 2}, {2, base_y + 3}]
+      # 田
       4 -> [{2, base_y}, {3, base_y}, {2, base_y + 1}, {3, base_y + 1}]
     end
   end
@@ -82,6 +89,7 @@ aoc 2022, 17 do
         end
       end)
 
+    # If valid pos, return OK with potential position. Otherwise return old position with an error.
     valid_pos = validate_position(locations, potential_pos)
     {valid_pos, if(valid_pos == :ok, do: potential_pos, else: rock)}
   end
--- lib/2022/18.ex
+++ lib/2022/18.ex
@@ -3,20 +3,16 @@ import AOC
 aoc 2022, 18 do
   def p1(input) do
     blocks = parse_input(input)
-    blocks
-    |> Enum.reduce(0, fn {x, y, z}, acc ->
-      # Determine if each of the current block's neighbors is in the mapset.
-      # If not, increment the accumulator for each side.
-      exposed =
-        [{x + 1, y, z}, {x - 1, y, z}, {x, y + 1, z}, {x, y - 1, z}, {x, y, z + 1}, {x, y, z - 1}]
-        |> Enum.map(fn neighbor -> if MapSet.member?(blocks, neighbor), do: 0, else: 1 end)
-        |> Enum.sum()
-      acc + exposed
-    end)
-    |> IO.inspect()
+    blocks |> count_sides_with_no_neighbors |> IO.inspect()
   end
 
   def p2(input) do
+    blocks = parse_input(input)
+    num_sides_no_neighbors = blocks |> count_sides_with_no_neighbors
+    graph = get_air_graph(blocks)
+    num_inner_sides = count_inner_sides(graph, blocks)
+
+    (num_sides_no_neighbors - num_inner_sides) |> IO.inspect()
   end
 
   def parse_input(input) do
@@ -27,5 +23,80 @@ aoc 2022, 18 do
       String.split(x, ",") |> Enum.map(&String.to_integer/1) |> List.to_tuple()
     end)
     |> MapSet.new()
+  end
+
+  @doc "Count the number of block sides with no neighbors"
+  def count_sides_with_no_neighbors(blocks) do
+    blocks
+    |> Enum.reduce(0, fn {x, y, z}, acc ->
+      # Determine if each of the current block's neighbors is in the mapset.
+      # If not, increment the accumulator for each side.
+      exposed =
+        [{x + 1, y, z}, {x - 1, y, z}, {x, y + 1, z}, {x, y - 1, z}, {x, y, z + 1}, {x, y, z - 1}]
+        |> Enum.map(fn neighbor -> if MapSet.member?(blocks, neighbor), do: 0, else: 1 end)
+        |> Enum.sum()
+
+      acc + exposed
+    end)
+  end
+
+  @doc "Given a set of blocks, create a graph of non-occupied (air) coordinates with other neighboring air coordinates as edges"
+  def get_air_graph(blocks) do
+    {min_x, max_x} = blocks |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+    {min_y, max_y} = blocks |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+    {min_z, max_z} = blocks |> Enum.map(&elem(&1, 2)) |> Enum.min_max()
+
+    graph = Graph.new()
+
+    graph =
+      Enum.reduce((min_x - 1)..(max_x + 1), graph, fn x, acc ->
+        Enum.reduce((min_y - 1)..(max_y + 1), acc, fn y, acc ->
+          Enum.reduce((min_z - 1)..(max_z + 1), acc, fn z, acc ->
+            if MapSet.member?(blocks, {x, y, z}), do: acc, else: Graph.add_vertex(acc, {x, y, z})
+          end)
+        end)
+      end)
+
+    Graph.vertices(graph)
+    |> Enum.reduce(graph, fn {x, y, z}, acc ->
+      neighbors = [
+        {x + 1, y, z},
+        {x - 1, y, z},
+        {x, y + 1, z},
+        {x, y - 1, z},
+        {x, y, z + 1},
+        {x, y, z - 1}
+      ]
+
+      neighbors
+      |> Enum.reduce(acc, fn neighbor, acc ->
+        if MapSet.member?(blocks, neighbor),
+          do: acc,
+          else: Graph.add_edge(acc, {x, y, z}, neighbor)
+      end)
+    end)
+  end
+
+  @doc "Given a graph of air, find ones which are not reachable from the inside and sum the number of adjacent blocks"
+  def count_inner_sides(graph, blocks) do
+    outer_blocks = Graph.reachable(graph, [{0, 0, 0}]) |> MapSet.new()
+
+    Graph.vertices(graph)
+    |> Enum.filter(fn x -> !MapSet.member?(outer_blocks, x) end)
+    |> Enum.reduce(0, fn {x, y, z}, acc ->
+      neighbors = [
+        {x + 1, y, z},
+        {x - 1, y, z},
+        {x, y + 1, z},
+        {x, y - 1, z},
+        {x, y, z + 1},
+        {x, y, z - 1}
+      ]
+
+      neighbors
+      |> Enum.reduce(acc, fn neighbor, acc ->
+        if MapSet.member?(blocks, neighbor), do: acc + 1, else: acc
+      end)
+    end)
   end
 end
