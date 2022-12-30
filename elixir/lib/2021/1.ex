a2d8c8e6ad8032c63e31d491d98647b08cc33cc0
Tim Trinidad <timtrinidad@gmail.com>
2022-12-29 16:17:15 -0800

    feat(2021-01): add challenge part 2 - 3.7ms

--- elixir/lib/2021/1.ex
+++ elixir/lib/2021/1.ex
@@ -2,16 +2,30 @@ import AOC
 
 aoc 2021, 1 do
   def p1(input) do
-    parse_input(input) |> Enum.map_reduce(9999999999999, fn x, acc ->
-      {(if x > acc, do: 1, else: 0), x}
-    end) |> elem(0) |> Enum.sum
-    |> IO.inspect
+    parse_input(input)
+    |> count_increasing(1)
+    |> IO.inspect()
   end
 
   def p2(input) do
+    parse_input(input)
+    |> count_increasing(3)
+    |> IO.inspect()
+  end
+
+  def count_increasing(list, window_size) do
+    init = for _ <- 1..window_size, do: 9_999_999_999_999
+
+    list
+    |> Enum.map_reduce(init, fn x, acc ->
+      next_window = tl(acc) ++ [x]
+      {if(Enum.sum(next_window) > Enum.sum(acc), do: 1, else: 0), next_window}
+    end)
+    |> elem(0)
+    |> Enum.sum()
   end
 
   def parse_input(input) do
-    input |> String.trim |> String.split("\n") |> Enum.map(&String.to_integer/1)
+    input |> String.trim() |> String.split("\n") |> Enum.map(&String.to_integer/1)
   end
 end
