6181dab5a4eceb35aac5219f2a962cd588d3347f
Tim Trinidad <timtrinidad@gmail.com>
2022-12-29 21:02:45 -0800

    feat(2021-02): add challenge part 2 - 1.8ms

--- elixir/lib/2021/2.ex
+++ elixir/lib/2021/2.ex
@@ -4,12 +4,27 @@ aoc 2021, 2 do
   def p1(input) do
     {h, d} =
       parse_input(input)
+      # Sum up each depth and horizontal position
       |> Enum.reduce({0, 0}, fn {h, d}, {acc_h, acc_d} -> {acc_h + h, acc_d + d} end)
 
     h * d
   end
 
   def p2(input) do
+    {h, d, _} =
+      parse_input(input)
+      |> Enum.reduce({0, 0, 0}, fn {h, d}, {acc_h, acc_d, acc_a} ->
+        {
+          # Sum up each horizontal position
+          acc_h + h,
+          # Add to depth the current aim times forward position
+          acc_d + acc_a * h,
+          # Set aim to be sum of "depth" of input
+          acc_a + d
+        }
+      end)
+
+    h * d
   end
 
   def parse_input(input) do
