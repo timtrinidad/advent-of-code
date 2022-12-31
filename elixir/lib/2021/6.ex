71bb5aa199a58b2d98508372d2171dc628771306
Tim Trinidad <timtrinidad@gmail.com>
2022-12-30 22:14:10 -0800

    feat(2021-06): add challenge part 2 - 3.48ms

--- elixir/lib/2021/6.ex
+++ elixir/lib/2021/6.ex
@@ -2,28 +2,35 @@ import AOC
 
 aoc 2021, 6 do
   def p1(input) do
-    fishes = parse_input(input)
-    # For each day, process each fish
-    1..80
-    |> Enum.reduce(fishes, fn _, fishes ->
-      {fishes, num_new_fish} =
-        fishes
-        |> Enum.map_reduce(0, fn fish, acc ->
-          # For every fish we wrap from 0 to 6, increment the number of new fish we should add
-          if fish == 0, do: {6, acc + 1}, else: {fish - 1, acc}
-        end)
-
-      # Append to the list the number of new fish to add. 1..1 adds one fish, 1..2 adds 2, etc.
-      # If we want no fish, 1..0 still generates two items (1 and 0). Force only positive
-      # increments with "//1"
-      fishes ++ for _ <- 1..num_new_fish//1, do: 8
-    end)
-    |> length
+    parse_input(input) |> simulate_spawning(80)
   end
 
   def p2(input) do
+    parse_input(input) |> simulate_spawning(256)
   end
 
+  @doc "Simulate the spawning for the given fish for any number of days"
+  def simulate_spawning(fishes, num_days) do
+    # For each day, process each fish
+    1..num_days
+    # Reduce the list of fish into a map of days left to number of fish
+    |> Enum.reduce(Enum.frequencies(fishes), fn day, fishes ->
+      fishes
+      # For each "day left", reduce the day. E.g. for 7 fish with 5 days, left, go from %{5 => 7} to %{4 => 7}
+      |> Enum.reduce(%{}, fn
+        # Account for possible key collisions - i.e. both 7 and 0 will resolve to 6
+        {0, num_fish}, acc ->
+          Map.update(acc, 6, num_fish, &(&1 + num_fish)) |> put_in([8], num_fish)
+
+        {days_left, num_fish}, acc ->
+          Map.update(acc, days_left - 1, num_fish, &(&1 + num_fish))
+      end)
+    end)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  @doc "Split input into a list of integers"
   def parse_input(input) do
     input |> String.trim() |> String.split(",") |> Enum.map(&String.to_integer/1)
   end
