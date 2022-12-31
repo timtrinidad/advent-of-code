7cd4c87906416817e301cb5a13ef1ed2ecb80ad2
Tim Trinidad <timtrinidad@gmail.com>
2022-12-30 20:31:40 -0800

    feat(2021-05): add challenge part 2 - 221ms

--- elixir/lib/2021/5.ex
+++ elixir/lib/2021/5.ex
@@ -4,17 +4,14 @@ aoc 2021, 5 do
   def p1(input) do
     parse_input(input)
     |> Enum.filter(fn [{x1, y1}, {x2, y2}] -> x1 == x2 || y1 == y2 end)
-    |> Enum.flat_map(fn [{x1, y1}, {x2, y2}] ->
-      for x <- x1..x2, y <- y1..y2, do: {x, y}
-    end)
-    |> Enum.frequencies()
-    |> Enum.filter(fn {_, freq} -> freq > 1 end)
-    |> length
+    |> count_overlapping_points
   end
 
   def p2(input) do
+    parse_input(input) |> count_overlapping_points
   end
 
+  @doc "Parse the input into a list of tuple pairs"
   def parse_input(input) do
     input
     |> String.trim()
@@ -27,5 +24,21 @@ aoc 2021, 5 do
         {String.to_integer(x), String.to_integer(y)}
       end)
     end)
+  end
+
+  @doc "Count the number of overlapping points for a given list of lines"
+  def count_overlapping_points(lines) do
+    lines
+    |> Enum.flat_map(fn [{x1, y1}, {x2, y2}] ->
+      if x1 == x2 || y1 == y2,
+        # For horizontal or vertical lines
+        do: for(x <- x1..x2, y <- y1..y2, do: {x, y}),
+        # For diagonal lines
+        else: Enum.zip(x1..x2, y1..y2)
+    end)
+    # Count frequencies of each coordinate, filter where more than 1, count number of results
+    |> Enum.frequencies()
+    |> Enum.filter(fn {_, freq} -> freq > 1 end)
+    |> length
   end
 end
