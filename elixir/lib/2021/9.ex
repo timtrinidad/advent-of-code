ec0343d3321ca231d7788ef3a3d014e716698c6a
Tim Trinidad <timtrinidad@gmail.com>
2022-12-31 17:13:31 -0800

    feat(2021-09): add challenge part 2 - 80ms

--- elixir/lib/2021/9.ex
+++ elixir/lib/2021/9.ex
@@ -4,13 +4,28 @@ aoc 2021, 9 do
   def p1(input) do
     elevations = parse_input(input)
 
-    find_low_points(elevations) |> Enum.map(fn coord ->
+    # For each low point, get the elevation plus 1 and sum them all
+    find_low_points(elevations)
+    |> Enum.map(fn coord ->
       Map.get(elevations, coord) + 1
-    end) |> Enum.sum
-
+    end)
+    |> Enum.sum()
   end
 
   def p2(input) do
+    elevations = parse_input(input)
+
+    # For each low point, find the size of its basin
+    low_points =
+      find_low_points(elevations)
+      |> Enum.map(fn coord ->
+        # Map to the size of the basin
+        find_basin(elevations, [coord], MapSet.new(), MapSet.new()) |> MapSet.size()
+      end)
+      # Get the product of the top three
+      |> Enum.sort(:desc)
+      |> Enum.take(3)
+      |> Enum.product()
   end
 
   @doc "Parse input into a map of elevations keyed by {x,y} coordinates"
@@ -39,10 +54,37 @@ aoc 2021, 9 do
       is_lowest =
         [{1, 0}, {-1, 0}, {0, 1}, {0, -1}]
         |> Enum.reduce(true, fn {dx, dy}, acc ->
+          # true && true if current elevation is lower than neighbor.
+          # If neighbor doesn't exist (edge), default to a really high elevation like 999
           acc && elevation < Map.get(elevations, {x + dx, y + dy}, 999)
         end)
 
+      # If it is the lowest, add to list of low points
       if is_lowest, do: [{x, y} | acc], else: acc
     end)
+  end
+
+  # Base case - no more nodes to explore in queue
+  def find_basin(_, [], _, basin), do: basin
+
+  @doc "DFS to find neighboring points which are part of the basin (not 9)"
+  def find_basin(elevations, [{x, y} | next], seen, basin) do
+    # Consider all four neighbors to explore. Don't explore again if already seen.
+    to_explore =
+      [{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}]
+      |> Enum.filter(fn coord -> !MapSet.member?(seen, coord) end)
+
+    # Mark current coordinates as seen
+    seen = MapSet.put(seen, {x, y})
+
+    {next, seen, basin} =
+      cond do
+        # If current point is 9, do nothing. Move onto next item in queue.
+        Map.get(elevations, {x, y}, 9) == 9 -> {next, seen, basin}
+        # If elevation is not 9, add valid unseen neighbors to the queue, and add this point to the basin
+        true -> {next ++ to_explore, seen, MapSet.put(basin, {x, y})}
+      end
+
+    find_basin(elevations, next, seen, basin)
   end
 end
