7d2ba8f516a25d64bbf2ccf07406be7d72eda634
Tim Trinidad <timtrinidad@gmail.com>
2023-01-01 23:04:23 -0800

    feat(2021-12): add challenge part 2 - 1.48s

--- elixir/lib/2021/12.ex
+++ elixir/lib/2021/12.ex
@@ -3,32 +3,29 @@ import AOC
 
 aoc 2021, 12 do
   def p1(input) do
-    parse_input(input) |> find_paths("start", MapSet.new(), [], []) |> length
+    parse_input(input) |> find_paths("start", false, [], []) |> length
   end
 
   def p2(input) do
-    parse_input(input)
+    parse_input(input) |> find_paths("start", true, [], []) |> length
   end
 
   # Base case - reached "end". Add "end" to the current path and add to the list of paths
-  def find_paths(_, "end", seen, path, paths), do: [["end" | path] | paths]
+  def find_paths(_, "end", _, path, paths), do: [["end" | path] | paths]
 
-  def find_paths(edges, curr, seen, path, paths) do
-    possible_destinations = edges[curr]
-    # Add to "seen" only if not capital letters
-    seen = if String.match?(curr, ~r/[A-Z]+/), do: seen, else: MapSet.put(seen, curr)
-
-    if length(possible_destinations) == 0 do
-      # No more possible destinations - return paths as is without adding current (invalid) path
-      paths
-    else
-      # For each possible destination that's not in "seen", explore that path
-      possible_destinations
-      |> Enum.filter(fn x -> !MapSet.member?(seen, x) end)
-      |> Enum.flat_map(fn to ->
-        find_paths(edges, to, seen, [curr | path], paths)
+  def find_paths(edges, curr, allow_mult_small?, path, paths) do
+    possible_destinations =
+      edges[curr]
+      |> Enum.filter(fn x ->
+        is_large?(x) || allow_mult_small? || x not in path
       end)
-    end
+
+    # For each possible destination that's not in "seen", explore that path
+    possible_destinations
+    |> Enum.flat_map(fn to ->
+      allow_mult_small? = allow_mult_small? && (is_large?(to) || to not in path)
+      find_paths(edges, to, allow_mult_small?, [curr | path], paths)
+    end)
   end
 
   @doc "Return a map of possible destinations keyed by vertex"
@@ -40,9 +37,11 @@ aoc 2021, 12 do
       [from, to] = String.split(line, "-")
 
       # Add to map both directions
+      acc = if to != "start", do: Map.update(acc, from, [to], fn x -> [to | x] end), else: acc
+      acc = if from != "start", do: Map.update(acc, to, [from], fn x -> [from | x] end), else: acc
       acc
-      |> Map.update(from, [to], fn x -> [to | x] end)
-      |> Map.update(to, [from], fn x -> [from | x] end)
     end)
   end
+
+  def is_large?(cave), do: String.match?(cave, ~r/[A-Z]+/)
 end
