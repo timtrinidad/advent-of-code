5262ab41f8a31c211c1cfc3046e2b5b8a4e7b24c
Tim Trinidad <timtrinidad@gmail.com>
2023-01-03 22:01:15 -0800

    feat(2021-16): add challenge part 2 - .27ms

--- elixir/lib/2021/16.ex
+++ elixir/lib/2021/16.ex
@@ -3,15 +3,16 @@ import AOC
 # https://adventofcode.com/2021/day/16
 aoc 2021, 16 do
   def p1(input) do
-    bits = parse_input(input)
-    {ver, val, _} = parse_packet(bits) |> IO.inspect()
+    {ver, val, _} = parse_input(input) |> parse_packet
     sum_versions({ver, val})
   end
 
   def p2(input) do
-    parse_input(input)
+    {ver, val, _} = parse_input(input) |> parse_packet
+    reduce_val({ver, val})
   end
 
+  @doc "Parse input string into a bitstring"
   def parse_input(input) do
     input |> :binary.decode_hex()
   end
@@ -23,40 +24,51 @@ aoc 2021, 16 do
     {version, bits_to_int(literal), rest}
   end
 
-  # Version is first three bits, second three bits is not 4 (discard), next bit is 0 (type length)
-  # next 15 bits is the length of the packets, and remainder of bits in "rest"
-  def parse_packet(<<version::3, _::3, 0::1, length_packets::15, rest::bitstring>>) do
-    # Parse the set of packets until remaining length is exhausted
-    {val, rest} = parse_package(:length, length_packets, rest, [])
-    {version, val, rest}
+  # Version is first three bits, second three bits designate the operator, and "rest" for remainder of bits
+  def parse_packet(<<version::3, operator::3, rest::bitstring>>) do
+    func =
+      case operator do
+        0 -> &Enum.sum/1
+        1 -> &Enum.product/1
+        2 -> &Enum.min/1
+        3 -> &Enum.max/1
+        5 -> &if Enum.at(&1, 0) > Enum.at(&1, 1), do: 1, else: 0
+        6 -> &if Enum.at(&1, 0) < Enum.at(&1, 1), do: 1, else: 0
+        7 -> &if Enum.at(&1, 0) == Enum.at(&1, 1), do: 1, else: 0
+      end
+
+    parse_operator(version, rest, func)
   end
 
-  # Version is first three bits, second three bits is not 4 (discard), next bit is 1 (type num)
-  # next 11 bits is num packets, remainer is in "rest"
-  def parse_packet(<<version::3, _::3, 1::1, num_packets::11, rest::bitstring>>) do
-    # Parse the set of packets until the remaining number of packets is exhausted
-    {val, rest} = parse_package(:num, num_packets, rest, [])
-    {version, val, rest}
+  # Parse the operands and store the proper operator function
+  def parse_operator(version, bits, func) do
+    {val, rest} = parse_operand(bits)
+    {version, {func, val}, rest}
+  end
+
+  # Base case for type length - no more bits to process.
+  def parse_operand(<<0::1, 0::15, rest::bitstring>>), do: {[], rest}
+
+  # First bit is 0 (type length), next 15 bits is the length of the packets, and remainder of bits in "rest"
+  def parse_operand(<<0::1, length_packets::15, rest::bitstring>>) do
+    orig_bit_size = bit_size(rest)
+    {version, val, rest} = parse_packet(rest)
+    processed_bit_size = orig_bit_size - bit_size(rest)
+
+    {acc, rest} =
+      parse_operand(<<0::1, length_packets - processed_bit_size::15, rest::bitstring>>)
+
+    {[{version, val}] ++ acc, rest}
   end
 
   # Base case - type "num" and no more packets to process - return accumulated values
-  def parse_package(:num, 0, rest, acc), do: {acc, rest}
-
-  # Parse the bits as packets until num_packages is exhausted
-  def parse_package(:num, num, bits, acc) do
-    {version, val, rest} = parse_packet(bits)
-    parse_package(:num, num - 1, rest, acc ++ [{version, val}])
-  end
-
-  # Base case - type "length" and no more bits to process - return accumulated values
-  def parse_package(:length, 0, rest, acc), do: {acc, rest}
-
-  # Parse the bits as packets until num_bits is exhausted
-  def parse_package(:length, length, bits, acc) do
-    orig_bit_size = bit_size(bits)
-    {version, val, rest} = parse_packet(bits)
-    processed_bit_size = orig_bit_size - bit_size(rest)
-    parse_package(:length, length - processed_bit_size, rest, acc ++ [{version, val}])
+  def parse_operand(<<1::1, 0::11, rest::bitstring>>), do: {[], rest}
+  # First bit is 1 (type num), next 11 bits is num packets, remainer is in "rest"
+  def parse_operand(<<1::1, num_packets::11, rest::bitstring>>) do
+    # Parse the set of packets until the remaining number of packets is exhausted
+    {version, val, rest} = parse_packet(rest)
+    {acc, rest} = parse_operand(<<1::1, num_packets - 1::11, rest::bitstring>>)
+    {[{version, val}] ++ acc, rest}
   end
 
   # Literal - first bit is 0 (this is the last set of 5), next 4 bits indicates a value
@@ -76,9 +88,13 @@ aoc 2021, 16 do
   end
 
   @doc "Recursively find and sum all version numbers"
-  def sum_versions({ver, val}) when is_list(val),
+  def sum_versions({ver, {_, val}}) when is_list(val),
     do: ver + Enum.sum(Enum.map(val, &sum_versions/1))
 
   # Base case - val is not a list, no more recursion. Simply return version.
   def sum_versions({ver, _}), do: ver
+
+  def reduce_val({_, {func, val}}) when is_list(val), do: func.(Enum.map(val, &reduce_val/1))
+  # Base case - val is not a list, no more recursion. Simply return value.
+  def reduce_val({_, val}), do: val
 end
