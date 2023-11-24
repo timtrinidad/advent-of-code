2b697809d0ff7bb79ad86d3d822af48180e6f5ef
Tim Trinidad <timtrinidad@gmail.com>
2023-11-24 15:05:09 -0800

    initiall commit haskell

---
+++ haskell/2022/01.hs
@@ -0,0 +1,27 @@
+#!/usr/bin/env stack
+{- stack script
+   --resolver lts-21.13
+   --package turtle
+   --package base
+   --package split
+-}
+{-# LANGUAGE OverloadedStrings #-}
+import Turtle (echo)
+import Data.List.Split (splitOn)
+import Data.List (sort)
+--import Common (readEntire)
+
+main :: IO()
+
+--convert_to_ints :: [String] -> [Int]
+--convert_to_ints ls = map read ls
+
+main = do
+  input <- readFile "inputs/01.sample.txt"
+  print input
+--  let input_lines = lines input
+--
+--  let intChunks = map convert_to_ints (splitOn [""] input_lines)
+--  let elfCalories = reverse $ sort $ map sum intChunks
+--  print $ head elfCalories
+--  print $ sum $ take 3 $ elfCalories
