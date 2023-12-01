14fd9545421aadf59a7cd76dd0b06424b3684274
Tim Trinidad <timothy.trinidad@powerschool.com>
2023-12-01 13:04:27 -0800

    feat(2022-04): add haskell challenge part 2 - 4.34ms

--- haskell/2022/days/AOC04.hs
+++ haskell/2022/days/AOC04.hs
@@ -11,7 +11,10 @@ part1 input = do
     $ filter (\[a, b, x, y] -> (a <= x && b >= y) || (x <= a && y >= b)) parsed
 
 part2 input = do
-  print "part2 not defined for day 04"
+  let parsed = parseInput input
+  -- Filter where both first set are before lower of second set or after higher of second set
+  let numNoOverlap = length $ filter(\[a, b, x, y] -> (a < x && b < x) || (a > y && b > y)) parsed
+  print $ (length parsed) - numNoOverlap
 
 parseInt x = read x :: Int
 parseInput = map(map(parseInt) . splitOneOf ",-") . lines
