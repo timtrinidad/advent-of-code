4344aeda0d3ecd83167b43384b7a7375edec31fd
Tim Trinidad <timothy.trinidad@powerschool.com>
2023-12-01 12:42:54 -0800

    feat(2022-03): add haskell challenge part 2 - 1.60ms

--- haskell/2022/days/AOC03.hs
+++ haskell/2022/days/AOC03.hs
@@ -6,21 +6,26 @@ import Data.Char (ord)
 
 day03 = (part1, part2)
 
+part1 input = do
+  -- Split each lint into two equal lines
+  let parsed = map(\x -> chunksOf (div (length x) 2) x) $ lines input -- Split into each line into halves
+  print $ intersectionPrioritySum $ parsed
+
+part2 input = do
+  -- Split into chunks of 3 lines
+  let parsed = chunksOf 3 $ lines input
+  print $ intersectionPrioritySum $ parsed
+
+-- Given a list of lists, find intersection, convert to priority, and sum priorities
+intersectionPrioritySum = sum . map(intersectionToPriority . foldr1 intersect)
+
+-- Converts a list like "ZZZ" to 52
+intersectionToPriority =
+  priority -- convert to priority
+  . ord -- get ASCII code
+  . head -- only first item in list
+
 priority x
   | x >= 97 = x - 96
   | otherwise = x - 38
 
-part1 input = do
-  let parsed = parseInput input
-  print $ sum $ map(
-    \[x, y] ->
-      priority -- convert to priority number
-      $ ord -- get ascii number
-      $ intersect x y !! 0 -- get first character from intersection
-    ) parsed
-
-part2 input = do
-  print "part2 not defined for day 03"
-
--- Split each line into two equa lists
-parseInput = map(\x -> chunksOf (div (length x) 2) x) . lines
