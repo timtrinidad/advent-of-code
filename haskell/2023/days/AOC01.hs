2f35f5980a0918277cdd9fe94033e1f705fd48ee
Tim Trinidad <timothy.trinidad@powerschool.com>
2023-11-30 21:12:13 -0800

    feat(2023-01): add haskell challenge part 1 - 8.32ms

--- haskell/2023/days/AOC01.hs
+++ haskell/2023/days/AOC01.hs
@@ -1,9 +1,16 @@
 module Days.AOC01 (day01) where
 
+import Data.Char
+
 day01 = (part1, part2)
 
 part1 input = do
-  print "part1 not defined for day 01"
+  let parsed = parseInput input
+  print $ sum $ map(\x -> parseInt [head x, last x]) parsed
 
 part2 input = do
   print "part2 not defined for day 01"
+
+parseInt = read::String->Int
+isInt x = ord x >= 48 && ord x <= 57
+parseInput = map(filter isInt) . lines
