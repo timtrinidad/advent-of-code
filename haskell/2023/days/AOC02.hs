668a098e9847b12795367228de24e09687f09ccf
Tim Trinidad <timothy.trinidad@powerschool.com>
2023-12-01 21:36:37 -0800

    feat(2023-02): add haskell challenge part 1 - 3.57ms

--- haskell/2023/days/AOC02.hs
+++ haskell/2023/days/AOC02.hs
@@ -1,9 +1,32 @@
 module Days.AOC02 (day02) where
 
+import Data.List.Split (splitOneOf, chunksOf)
+
 day02 = (part1, part2)
 
 part1 input = do
-  print "part1 not defined for day 02"
+  let parsed = parseInput input
+  -- Find all games that are invalid - for each game, filter to only invalid draws and then filter
+  -- to only games with any invalid draws
+  let invalidGames = filter (\(gameNum, draws) -> (length(filter(not . isValid) draws) > 0)) parsed
+  let sumInvalidGameIds = sum $ map(fst) invalidGames
+  -- Find the inverse - sum of valid games
+  print $ sum [1..length parsed] - sumInvalidGameIds
 
 part2 input = do
   print "part2 not defined for day 02"
+
+isValid [num, "red"] = parseInt num <= 12
+isValid [num, "green"] = parseInt num <= 13
+isValid [num, "blue"] = parseInt num <= 14
+
+-- Results in an input like [(1, [["12", "red"], ["2", "green"], ["5", "green"]]), (2, [...])]
+parseInput = map(\x -> do
+    let [gameNum, sets] = splitOneOf ":" x
+    let cubes = chunksOf 2 $ filter(not . null) $ splitOneOf ";, " $ sets
+    (parseInt $ drop 5 gameNum, cubes)
+  ) . lines
+
+
+parseInt = read :: String->Int
+
