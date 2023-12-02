fdf719e8d05fa82e52d5c6df0809d3a64d7adbc4
Tim Trinidad <timothy.trinidad@powerschool.com>
2023-12-01 21:54:05 -0800

    feat(2023-02): add haskell challenge part 2 - 3.83ms

--- haskell/2023/days/AOC02.hs
+++ haskell/2023/days/AOC02.hs
@@ -14,16 +14,37 @@ part1 input = do
   print $ sum [1..length parsed] - sumInvalidGameIds
 
 part2 input = do
-  print "part2 not defined for day 02"
+  let parsed = parseInput input
+  --
+  print $ sum $ map(product . findMaxPerColor) parsed
 
 isValid [num, "red"] = parseInt num <= 12
 isValid [num, "green"] = parseInt num <= 13
 isValid [num, "blue"] = parseInt num <= 14
 
+-- Returns a list of the max number of colors for a given game
+findMaxPerColor :: (Int, [[String]]) -> [Int]
+findMaxPerColor (_, draws) =
+  -- For each color
+  map(\color ->
+    -- Reduce to the max
+    maximum
+    -- Get the number of that color
+    $ map(\[num, _] -> parseInt num)
+    -- Filter all num/color pairs to the selected color
+    $ filter(\x -> x !! 1 == color)
+    draws
+  )
+  ["red", "green", "blue"]
+
 -- Results in an input like [(1, [["12", "red"], ["2", "green"], ["5", "green"]]), (2, [...])]
+parseInput :: String -> [(Int, [[String]])]
 parseInput = map(\x -> do
+    -- Separate out the game number
     let [gameNum, sets] = splitOneOf ":" x
+    -- Difference between colors and draws don't matter - get a list of number/color pairs in the whole game
     let cubes = chunksOf 2 $ filter(not . null) $ splitOneOf ";, " $ sets
+    -- Tuple with game number as int and array of num/color pairs
     (parseInt $ drop 5 gameNum, cubes)
   ) . lines
 
