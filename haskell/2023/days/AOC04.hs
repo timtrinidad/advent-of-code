95c0ffc78e950447414c44c305b7646a5e85d360
Tim Trinidad <timtrinidad@gmail.com>
2023-12-03 22:46:28 -0800

    feat(2023-04): add haskell challenge part 1 - 16ms

--- haskell/2023/days/AOC04.hs
+++ haskell/2023/days/AOC04.hs
@@ -1,9 +1,32 @@
 module Days.AOC04 (day04) where
 
+import Data.List.Split (splitOneOf, chunksOf, splitOn)
+import Data.List (intersect)
+import Common (parseInt)
+
 day04 = (part1, part2)
 
 part1 input = do
-  print "part1 not defined for day 04"
+  let parsed = parseInput input
+  print
+    $ sum
+    -- Convert to points based
+    $ map (\x -> if x > 0 then 2 ^ (x-1) else 0)
+    -- For each line map to # of intersections between left and right
+    $ map (\[x, y] -> (length $ intersect x y)) parsed
 
 part2 input = do
   print "part2 not defined for day 04"
+
+-- Split each line into two lists of numbers
+parseInput =
+  map (
+      map (
+          map parseInt . chunksOf 3
+        )
+      . splitOn " |"
+      -- Drop the "game" part
+      . drop 1
+      . dropWhile (/= ':')
+    )
+  . lines
