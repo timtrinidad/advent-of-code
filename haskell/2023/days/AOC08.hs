edd231b54c22b1defbba73f94e17acd18b34ec28
Tim Trinidad <timtrinidad@gmail.com>
2023-12-08 00:57:05 -0800

    feat(2023-08): add haskell challenge part 2 - 532ms

--- haskell/2023/days/AOC08.hs
+++ haskell/2023/days/AOC08.hs
@@ -1,27 +1,39 @@
 module Days.AOC08 (day08) where
 
+import Data.List (foldl1)
 import Data.List.Split (splitOn, splitOneOf)
 import Data.Maybe (fromJust)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Debug.Trace
 
 day08 = (part1, part2)
 
+-- Traverse the map recursively starting with "AAA"
 part1 input = show $ traverseMap mapping dir "AAA" 0
   where
     (dir, mapping) = parseInput input
 
+-- Find the least common multiple of the number of steps it takes
+-- to get from each staring point (anything ending with 'A') to
+-- their corresponding ending point (ending with 'Z')
+part2 input = show $ foldl1 lcm indivNumsToEnd
+  where
+    indivNumsToEnd = map (\x -> traverseMap mapping dir x 0) startingLocs -- each starting piont's # steps
+    startingLocs = filter (endsIn 'A') $ Map.keys mapping -- Find points ending with 'A'
+    (dir, mapping) = parseInput input
+
 -- Traverse the map iterating through each direction until we get to "ZZZ"
-traverseMap _ _ "ZZZ" numMoves = numMoves -- base case
-traverseMap mapping (currDir:restDir) currLoc numMoves = traverseMap mapping dirs nextLoc (numMoves+1)
+traverseMap mapping (currDir:restDir) currLoc numMoves
+  | endsIn 'Z' currLoc = numMoves -- base case
+  | otherwise = traverseMap mapping dirs nextLoc (numMoves+1) -- recurse
   where
     dirs = (restDir ++ [currDir]) -- append current dir back to the end of the list
     nextLoc = (if currDir == 'R' then snd else fst) currToNextLoc -- pick second direction if 'R' otherwise first direction
     currToNextLoc = fromJust $ Map.lookup currLoc mapping -- mapping lookup - throw error if not found - shouldn't happen
 
-
-part2 input = do
-  show "part2 not defined for day 08"
+-- Return T/F if the string ends in the given letter
+endsIn letter str = last str == letter
 
 -- Parse the dir and mapping separately
 parseInput input = (dir, parseMapping mapping)
