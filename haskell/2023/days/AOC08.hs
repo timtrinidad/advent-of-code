79442722060d25fffde95ca9c2559f50658d5548
Tim Trinidad <timtrinidad@gmail.com>
2023-12-08 11:21:39 -0800

    feat(2023-08): improvement to haskell challenge - use 'scanl'

--- haskell/2023/days/AOC08.hs
+++ haskell/2023/days/AOC08.hs
@@ -1,6 +1,6 @@
 module Days.AOC08 (day08) where
 
-import Data.List (foldl1)
+import Data.List (foldl1, isSuffixOf, scanl)
 import Data.List.Split (splitOn, splitOneOf)
 import Data.Maybe (fromJust)
 import Data.Map (Map)
@@ -10,34 +10,26 @@ import Debug.Trace
 day08 = (part1, part2)
 
 -- Traverse the map recursively starting with "AAA"
-part1 input = show $ traverseMap mapping dir "AAA" 0
-  where
-    (dir, mapping) = parseInput input
+part1 input = show $ traverseMap mapping dir "AAA"
+  where (dir, mapping) = parseInput input
 
 -- Find the least common multiple of the number of steps it takes
 -- to get from each staring point (anything ending with 'A') to
 -- their corresponding ending point (ending with 'Z')
-part2 input = show $ foldl1 lcm indivNumsToEnd
-  where
-    indivNumsToEnd = map (\x -> traverseMap mapping dir x 0) startingLocs -- each starting piont's # steps
-    startingLocs = filter (endsIn 'A') $ Map.keys mapping -- Find points ending with 'A'
-    (dir, mapping) = parseInput input
+part2 input = show
+    $ foldl1 lcm
+    $ map (traverseMap mapping dir) -- each starting point's # steps to '..Z'
+    $ filter (isSuffixOf "A") $ Map.keys mapping -- Find points ending with 'A'
+  where (dir, mapping) = parseInput input
 
--- Traverse the map iterating through each direction until we get to "ZZZ"
-traverseMap mapping (currDir:restDir) currLoc numMoves
-  | endsIn 'Z' currLoc = numMoves -- base case
-  | otherwise = traverseMap mapping restDir nextLoc (numMoves+1) -- recurse
-  where
-    nextLoc = (if currDir == 'R' then snd else fst) currToNextLoc -- pick second direction if 'R' otherwise first direction
-    currToNextLoc = fromJust $ Map.lookup currLoc mapping -- mapping lookup - throw error if not found - shouldn't happen
-
--- Return T/F if the string ends in the given letter
-endsIn letter str = last str == letter
+-- Traverse the map iterating through each direction until we get to something that ends with 'Z'
+traverseMap mapping dirs currLoc = length $ takeWhile (not . isSuffixOf "Z") $ scanl getNextLoc currLoc dirs
+  -- pick second direction if 'R' otherwise first direction
+  where getNextLoc currLoc dir = (if dir == 'R' then snd else fst) $ mapping Map.! currLoc
 
 -- Parse the dir and mapping separately
 parseInput input = (cycle dir, parseMapping mapping)
-  where
-    [dir, mapping] = splitOn "\n\n" input
+  where [dir, mapping] = splitOn "\n\n" input
 
 -- Convert each line into a map entry (first is the key, second and third is the value as a tuple
 parseMapping input = Map.fromList $ processLine <$> splitOn "\n" input
