194db94475720cb267bd2b234a3c155201716cd0
Tim Trinidad <timtrinidad@gmail.com>
2023-12-08 23:07:03 -0800

    feat(2023-09): add haskell challenge part 1 - 23.15ms

--- haskell/2023/days/AOC09.hs
+++ haskell/2023/days/AOC09.hs
@@ -1,9 +1,23 @@
 module Days.AOC09 (day09) where
 
+import Data.List (zip, nub)
+import Data.List.Split (splitOn)
+import Common (parseInt)
+import Debug.Trace
+
 day09 = (part1, part2)
 
-part1 input = do
-  show "part1 not defined for day 09"
+part1 input = show $ sum $ map findNext $ parseInput input
 
 part2 input = do
   show "part2 not defined for day 09"
+
+findNext list
+  | (length $ nub list) == 1 = head list
+  | otherwise = last list + nextNum
+  where
+    diffs (a, b) = b - a
+    nextNum = findNext $ map diffs $ zip list $ tail list
+
+parseInput = map processLine . lines
+  where processLine = map parseInt . splitOn " "
