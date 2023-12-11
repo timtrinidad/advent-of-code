876685248e7485d4245a4f6bbe55ac1370ccaa4d
Tim Trinidad <timtrinidad@gmail.com>
2023-12-11 06:49:52 -0800

    feat(2023-11): add haskell challenge part 1 - 1115ms

--- haskell/2023/days/AOC11.hs
+++ haskell/2023/days/AOC11.hs
@@ -1,9 +1,42 @@
 module Days.AOC11 (day11) where
 
+-- import Data.List.Split (splitAt)
+import Data.List (foldr, transpose)
+import Debug.Trace
+
 day11 = (part1, part2)
 
-part1 input = do
-  show "part1 not defined for day 11"
+-- Sum the manhattan distances between all pairs of galaxies
+part1 input = show $ sum distances
+  where
+    distances = map (\(i, j) -> calcDistance emptyRows emptyCols (galaxies !! i) (galaxies !! j)) pairs -- List of distances of each pair
+    pairs = [(i, j) | i <- [0..numGalaxies-1], j <- [0..numGalaxies-1], i < j] -- all index pairs ignoring order
+    numGalaxies = length galaxies
+    (galaxies, emptyRows, emptyCols) = parseInput input
+
+-- Calculate the manhattan distance between two points, adding padding based on the empty columns/rows
+calcDistance emptyRows emptyCols (x1, y1) (x2, y2) =  dx + colPadding + dy + rowPadding
+  where
+    dx = abs (x2 - x1) -- diff of xs
+    dy = abs (y2 - y1) -- diff of ys
+    colPadding = length $ filter (\col -> col > min y1 y2 && col < max y1 y2) emptyCols -- num cols between y points
+    rowPadding = length $ filter (\row -> row > min x1 x2 && row < max x1 x2) emptyRows -- num rows between x points
+
 
 part2 input = do
   show "part2 not defined for day 11"
+
+-- Parse the input into 
+--   - a list of points of galaxies in the unexpanded map
+--   - a list of empty column indices
+--   - a list of empty row indices.
+parseInput input = (galaxyPoints, emptyCols, emptyRows)
+  where
+    emptyCols = emptyIdxs $ transpose grid -- list of empty column indices
+    emptyRows = emptyIdxs grid -- List of empty row indices
+    emptyIdxs = map fst . filter isEmpty . zip [0..] -- Indices of empty lines
+    isEmpty = all (== '.') . snd -- Entire line is periods
+    galaxyPoints = concatMap processRow $ zip [0..] $ grid -- list of points of galaxies in unexpanded map
+    processRow (y, row) = map (processCol y) $ filter (\(_, char) -> char /= '.') $ zip [0..] row -- filter out periods and extract coordinates
+    processCol y (x, _) = (x, y) -- convert the rol/col nums into an x,y point, ignoring the actual character
+    grid = lines input -- split into lines
