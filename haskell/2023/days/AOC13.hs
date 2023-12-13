d6e751aee516f3b5723617842ffb8d4c8942da97
Tim Trinidad <timtrinidad@gmail.com>
2023-12-13 08:29:42 -0800

    feat(2023-13): add haskell challenge part 2 - 35.26ms

--- haskell/2023/days/AOC13.hs
+++ haskell/2023/days/AOC13.hs
@@ -3,45 +3,45 @@ module AOC13 (day13) where
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.List (find)
+import Data.List (find, transpose)
 import Debug.Trace
-
-data VertexType = Vertical | Horizontal deriving (Read, Show, Enum, Eq, Ord)
 
 day13 :: (String -> String, String -> String)
 day13 = (part1, part2)
 
--- Sum each graph's vertex vaule
+-- Sum each graph's vertex value
 part1 :: String -> String
-part1 input = show $ sum $ map findVertices graphs
+part1 input = show $ sum $ graphValues 0 graphs
+  where graphs = parseInput input
+
+-- Sum each graph's vertext value allowing for 1 incorrect match on reflections
+part2 :: String -> String
+part2 input = show $ sum $ graphValues 1 graphs
+  where graphs = parseInput input
+
+graphValues :: Int -> [(Map (Int, Int) Char, (Int, Int))] -> [Int]
+graphValues numIncorrectAllowed = map findVertices
   where
     -- Multiply vertices as necessary - only one vertical or horizontal should be found
     -- so one side of + should always be 0
-    findVertices graph = maybe 0 (+1) (verticalVtx graph) -- no multiplication
-      + maybe 0 (\x -> (x+1)*100) (horizontalVtx graph) -- multiply by 100
-    verticalVtx = findVertex Vertical
-    horizontalVtx = findVertex Horizontal
-    graphs = parseInput input
-
-part2 :: String -> String
-part2 input = do
-  show "part2 not defined for day 13"
-
+    findVertices graph = maybe 0 (+1) (findVertex' graph) -- no multiplication
+      + maybe 0 (\x -> (x+1)*100) (findVertex' $ transposed graph) -- multiply by 100
+    findVertex' = findVertex numIncorrectAllowed
+    -- Rotate a graph 90 degrees
+    transposed (diagram, (width, height)) = (Map.mapKeys (\(x, y) -> (y, x)) diagram, (height, width))
 
 -- Iterate through all possible vertices looking for a reflection
-findVertex :: VertexType -> (Map (Int, Int) Char, (Int, Int)) -> Maybe Int
-findVertex vertexType (diagramMap, dimensions) = find isValidVertex [0..idx dimensions-2]
+findVertex :: Int -> (Map (Int, Int) Char, (Int, Int)) -> Maybe Int
+findVertex numIncorrectAllowed (diagramMap, (width, _)) = find isValidVertex [0..width-2]
   where
-    -- The vertex at a given index is valid if all the points across the reflection are equal (or empty)
-    isValidVertex vtx =  and $ Map.elems $ Map.mapWithKey (checkReflection vtx) $ Map.filterWithKey (\point _ -> idx point <= vtx) diagramMap
+    -- Is valid if no unmatched (part 1) or only 1 unmatched (part 2)
+    isValidVertex vtx =  numIncorrectAllowed == numIncorrectMatches vtx
+    -- Find the number of points that don't match
+    numIncorrectMatches vtx = length $ filter not $ Map.elems $ Map.mapWithKey (checkReflection vtx) $ Map.filterWithKey (\(x, _) _ -> x <= vtx) diagramMap
     -- Check of the reflected point is the same charachter
-    checkReflection vtx point char = char == Map.findWithDefault char (reflect vtx point) diagramMap
-    reflect = reflectAcross vertexType -- function to reflect across the vertex type
-    idx = if vertexType == Horizontal then snd else fst -- (x, y) - horizontal line is second, vertical is first
+    checkReflection vtx point char = char == Map.findWithDefault char (reflectAcross vtx point) diagramMap
+    reflectAcross vtx (x, y) = (2*vtx+1 - x, y) -- Reflect a point across a given vertex
 
--- Reflect a point across a given vertex
-reflectAcross Vertical vtx (x, y) = (2*vtx+1 - x, y)
-reflectAcross Horizontal vtx (x, y) = (x, 2*vtx+1 - y)
 
 -- Split into multiple diagrams and process each
 parseInput :: String -> [(Map (Int, Int) Char, (Int, Int))]
