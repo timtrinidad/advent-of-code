19fe042a5ca79f1b4825614807d04fa33da437d7
Tim Trinidad <timtrinidad@gmail.com>
2023-12-14 14:39:49 -0800

    feat(2023-14): add haskell challenge part 2 - 32.49ms

--- haskell/2023/days/AOC12.hs
+++ haskell/2023/days/AOC12.hs
@@ -10,79 +10,30 @@ day12 = (part1, part2)
 
 -- For each record, get the number of possible arragements, and sum
 part1 :: String -> String
-<<<<<<< Updated upstream
-part1 input = show $ sum $ map numArrangements records 
-=======
 part1 input = show $ sum $ map numArrangements $ parseInput 1 input
->>>>>>> Stashed changes
-  where
     
 
 part2 :: String -> String
-<<<<<<< Updated upstream
-part2 input = show $ sum $ map handleRecord $ zip [1..] records
-  where 
-    handleRecord (idx, record) = traceShow idx $ numArrangements record
-    records = parseInput 5 input
-    -- 4 - 4.7s
-=======
 part2 input = show $ sum $ zipWith (curry handleRecord) [1..] $ parseInput 5 input
   where
     handleRecord (idx, record) = traceShow idx $ numArrangements record
->>>>>>> Stashed changes
 
 
 -- For a given diagram and list of working groups, count the total number of valid arrangements
 numArrangements (diagram, workingGroups) =
       length
-<<<<<<< Updated upstream
-      $ filter (==(workingGroups record)) -- filter to ones that match the nums for this record
-=======
       -- $ traceShow filteredOut
       $ filter (==workingGroups) -- filter to ones that match the nums for this record
->>>>>>> Stashed changes
       $ map convertToNums allPossibilities -- convert to a number record
   where
     allPossibilities = expandPossibilities diagram workingGroups -- generatate all possible combinations
     convertToNums = map length . wordsBy (=='.') -- remove periods and count each group of '#'
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 -- Recursively generate possibilties, converting each ? into two possible strings (# or .)
 expandPossibilities :: String -> [Int] -> [String]
 expandPossibilities [] _ = [""]
 expandPossibilities (currSpring:restSprings) workingGroups = do notWorking ++ working
   where
-<<<<<<< Updated upstream
-    expansionBroken = if isValid record $ reverse optionBroken then expandPossibilities record xs optionBroken else []
-    optionBroken = '.':partialDiagram
-    expansionWorking = if isValid record $ reverse optionWorking then expandPossibilities record xs optionWorking else []
-    optionWorking = '#':partialDiagram
-expandPossibilities record (x:xs) partialDiagram = expandPossibilities record xs (x:partialDiagram)
-
-isValid record partialDiagram = -- traceShow (diagram record) $ traceShow (partialDiagram)
-    and [
-        notTooManyBroken, 
-        notTooManyWorking, 
-        matchesWorkingGroups, 
-        notTooManyWorkingGroups,
-        notTooManyWorkingGroupsLeft,
-        lastGroupNotTooLarge
-      ]
-  where
-    notTooManyWorkingGroupsLeft = length partialDiagram + toBeAllocated <= numTotal record
-    toBeAllocated = sum workingGroupsLeft + (length workingGroupsLeft - 1)
-    workingGroupsLeft = drop (length partialWorkingGroups + 1) workingGroups'
-    notTooManyWorkingGroups = length partialWorkingGroups <= numWorkingGroups record
-    notTooManyBroken = (numBroken record) >= (length $ filter (=='.') partialDiagram)
-    notTooManyWorking = (numWorking record) >= (length $ filter (=='#') partialDiagram)
-    lastGroupNotTooLarge = if last partialDiagram == '.' then True else (length (takeWhile (=='#') $ reverse partialDiagram)) <= (head $ drop (length partialWorkingGroups) workingGroups')
-    matchesWorkingGroups = and $ zipWith (==) workingGroups' partialWorkingGroups
-    workingGroups' = workingGroups record
-    partialWorkingGroups = (if last partialDiagram == '.' then id else init) $ map length $ wordsBy (=='.') partialDiagram
-=======
     notWorking = if currSpring == '#' then [] else map ('.':) $ expandPossibilities restSprings workingGroups
     working = expandWorkingPossibilities (currSpring:restSprings) workingGroups
 
@@ -96,28 +47,9 @@ expandWorkingPossibilities diagram (currWorkingGroup:restWorkingGroups)
     nextCharsAreAvalid numWorking diagram' = all (`elem` ['#', '?']) $ take numWorking diagram'
     hasEnoughCharsLeft numWorking diagram' = numWorking <= length diagram'
     substring = expandPossibilities (drop currWorkingGroup diagram) restWorkingGroups
->>>>>>> Stashed changes
 
 parseInput :: Int -> String -> [(String, [Int])]
 parseInput multiplier = map processLine . lines
-<<<<<<< Updated upstream
-  where processLine = toRecord multiplier . splitOn " " -- split by space
-
-toRecord :: Int -> [String] -> Record
-toRecord multiplier [diagramOriginal, nums] = Record {
-  diagram = diagram',
-  workingGroups = workingGroups',
-  numWorkingGroups = length workingGroups',
-  numWorking = numWorking',
-  numTotal = length diagram',
-  numBroken = length diagram' - numWorking',
-  numKnownBroken = length $ filter (=='.') diagram'
-} where 
-    diagram' = intercalate "?" $ replicate multiplier diagramOriginal
-    workingGroups' = concat $ replicate multiplier $ map parseInt $ splitOn "," nums
-    numWorking' = sum workingGroups'
-=======
   where
     processLine = parseNums . splitOn " " -- split by space
     parseNums [slots, nums] = (intercalate "?" $ replicate multiplier slots, concat $ replicate multiplier $ map parseInt $ splitOn "," nums) -- convert right side to list of ints
->>>>>>> Stashed changes
--- haskell/2023/days/AOC14.hs
+++ haskell/2023/days/AOC14.hs
@@ -2,35 +2,70 @@ module AOC14 (day14) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Maybe (fromMaybe)
-import Data.List (find)
+import Data.Maybe (fromMaybe, fromJust)
+import Data.List (find, group, sort)
 import Debug.Trace
+
+data Direction = North | West | South | East
+  deriving (Eq, Ord, Show)
+
 
 day14 = (part1, part2)
 
 -- Get the sum of all row numbers for each rolling rock
 part1 :: String -> String
-part1 input = show $ sum rollingRockRows
+part1 input = show $ calculateLoad height finalMapping
   where
-    -- Find all rolling rocks and map to their inverted height (highest one has highest row index)
-    rollingRockRows = map (\(_, y) -> height - y) $ Map.keys $ Map.filter (=='O') finalMapping
-    -- Map containing final location of rocks
-    finalMapping = foldl moveRock mapping $ [(x, y) | x <- [0..width-1], y <- [0..height-1]]
+    finalMapping = moveRocks North (width, height) mapping -- final location of all rocks
     (mapping, (width, height)) = parseInput input
 
+-- Find the number at the given index in the pattern, accounting for the skip
 part2 :: String -> String
-part2 input = do
-  show "part2 not defined for day 14"
+part2 input = show $ loads !! (skip + patternIdx) 
+  where
+    -- Apply the pattern to the number of cycles needed
+    patternIdx = (1000000000 - skip - 1) `mod` patternLength
+    -- Find a patten - iteratively try different skip and pattern lengths until a pattern is found
+    -- (skip x, extract the next to y's to see if they match)
+    (skip, patternLength) = fromJust $ find (\(x, y) -> take y (drop x loads) == take y (drop (x+y) loads)) [(x, y) | x <- [0..1000], y <- [2..1000]]
+    -- Only keep every fourth one (a full cycle)
+    loads = map (calculateLoad height . snd) $ filter (\(x, _) -> x `mod` 4 == 3) $ zip [0..] allMappings
+    -- process the mapping tracking the result each time
+    allMappings = scanl (\acc dir -> moveRocks dir (width, height) acc) mapping directions 
+    -- replicate 1000 times so we can find a pattern
+    directions = concat $ replicate 1000 [North, West, South, East] 
+    (mapping, (width, height)) = parseInput input
+
+-- Find all rolling rocks and map to their inverted height (highest one has highest row index)
+calculateLoad :: Int -> Map (Int, Int) Char -> Int
+calculateLoad height mapping = sum $ map (\(_, y) -> height - y) $ Map.keys $ Map.filter (=='O') mapping
+
+-- Update a mapping by shifting all rolling rocks in a given direction
+moveRocks direction (width, height) mapping = foldl moveRock' mapping allPoints'
+  where
+    allPoints' = (if direction `elem` [North, West] then id else reverse) allPoints
+    allPoints = [(x, y) | x <- [0..width-1], y <- [0..height-1]]
+    moveRock' = moveRock direction (width, height)
 
 -- Move a rock as high as possible, returning a new Map after the move.
-moveRock :: Map (Int, Int) Char -> (Int, Int) -> Map (Int, Int) Char
-moveRock mapping (currX, currY)
-  | Map.findWithDefault '#' (currX, currY) mapping == '#' = mapping -- do nothing
+moveRock :: Direction -> (Int, Int) -> Map (Int, Int) Char -> (Int, Int) -> Map (Int, Int) Char
+moveRock dir (width, height) mapping (currX, currY)
+  | Map.findWithDefault '#' (currX, currY) mapping == '#' = mapping -- do nothing - can't move a '#'
   | otherwise = Map.insert newPoint 'O' $ Map.delete (currX, currY) mapping -- replace with new 
   where
-    newPoint = (currX, obstacleY + 1) -- New point is one aftet the obstacle
+    -- New point is one aftet the obstacle
+    newPoint = case dir of
+      North -> (currX, obstacleY + 1)
+      West  -> (obstacleX + 1, currY)
+      South -> (currX, obstacleY - 1)
+      East  -> (obstacleX - 1, currY)
     -- Move up one by one until we hit an obstacle
-    obstacleY = fromMaybe (-1) $ find (\y -> Map.member (currX, y) mapping) [currY-1,currY-2..(-2)]
+    obstacleY = case dir of
+      North -> fromMaybe (-1) $ find (\y -> Map.member (currX, y) mapping) [currY-1,currY-2..(-2)]
+      South -> fromMaybe height $ find (\y -> Map.member (currX, y) mapping) [currY+1,currY+2..(height+1)]
+    obstacleX = case dir of
+      West -> fromMaybe (-1) $ find (\x -> Map.member (x, currY) mapping) [currX-1,currX-2..(-2)]
+      East -> fromMaybe width $ find (\x -> Map.member (x, currY) mapping) [currX+1,currX+2..(width+1)]
 
 -- Parse the input string into a Map and dimensions
 parseInput :: String -> (Map (Int, Int) Char, (Int, Int))
