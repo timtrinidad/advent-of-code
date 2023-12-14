bb10218e2d8970c0633552701b6f3a11c4449cdb
Tim Trinidad <timtrinidad@gmail.com>
2023-12-13 16:34:23 -0800

    temp

--- haskell/2023/days/AOC12.hs
+++ haskell/2023/days/AOC12.hs
@@ -19,44 +19,53 @@ data Record = Record {
 
 -- For each record, get the number of possible arragements, and sum
 part1 :: String -> String
-part1 input = show $ sum $ map numArrangements records 
+part1 input = show $ sum $ map numArrangements records
   where
     records = parseInput 1 input
 
 part2 :: String -> String
-part2 input = show $ sum $ map handleRecord $ zip [1..] records
-  where 
+part2 input = show $ sum $ zipWith (curry handleRecord) [1..] records
+  where
+    handleRecord :: (Int, Record) -> Int
     handleRecord (idx, record) = traceShow idx $ numArrangements record
     records = parseInput 5 input
-    -- 4 - 4.7s
 
 
 -- For a given diagram and list of working groups, count the total number of valid arrangements
 numArrangements :: Record -> Int
 numArrangements record =
       length
-      $ filter (==(workingGroups record)) -- filter to ones that match the nums for this record
+      -- $ traceShow filteredOut
+      $ filter (==workingGroups record) -- filter to ones that match the nums for this record
       $ map convertToNums allPossibilities -- convert to a number record
   where
     allPossibilities = expandPossibilities record (diagram record) "" -- generatate all possible combinations
     convertToNums = map length . wordsBy (=='.') -- remove periods and count each group of '#'
+    filteredOut = filter (/=workingGroups record) $ map convertToNums allPossibilities
 
 -- Recursively generate possibilties, converting each ? into two possible strings (# or .)
 expandPossibilities :: Record -> String -> String -> [String]
 expandPossibilities _ [] partialDiagram = [reverse partialDiagram]
 expandPossibilities record ('?':xs) partialDiagram = expansionBroken ++ expansionWorking
   where
-    expansionBroken = if isValid record $ reverse optionBroken then expandPossibilities record xs optionBroken else []
+    expansionBroken = if isValid record optionBroken then expandPossibilities record xs optionBroken else []
     optionBroken = '.':partialDiagram
-    expansionWorking = if isValid record $ reverse optionWorking then expandPossibilities record xs optionWorking else []
+    expansionWorking = if isValid record optionWorking then expandPossibilities record xs optionWorking else []
     optionWorking = '#':partialDiagram
 expandPossibilities record (x:xs) partialDiagram = expandPossibilities record xs (x:partialDiagram)
 
-isValid record partialDiagram = -- traceShow (diagram record) $ traceShow (partialDiagram)
+isValid record partialDiagram = --traceShowId $ traceShow (diagram record, reverse partialDiagram, workingGroups record, [
+      --   notTooManyBroken,
+      --   notTooManyWorking,
+      --   matchesWorkingGroups,
+      --   notTooManyWorkingGroups,
+      --   notTooManyWorkingGroupsLeft,
+      --   lastGroupNotTooLarge
+      -- ])
     and [
-        notTooManyBroken, 
-        notTooManyWorking, 
-        matchesWorkingGroups, 
+        notTooManyBroken,
+        notTooManyWorking,
+        matchesWorkingGroups,
         notTooManyWorkingGroups,
         notTooManyWorkingGroupsLeft,
         lastGroupNotTooLarge
@@ -66,12 +75,12 @@ isValid record partialDiagram = -- traceShow (diagram record) $ traceShow (parti
     toBeAllocated = sum workingGroupsLeft + (length workingGroupsLeft - 1)
     workingGroupsLeft = drop (length partialWorkingGroups + 1) workingGroups'
     notTooManyWorkingGroups = length partialWorkingGroups <= numWorkingGroups record
-    notTooManyBroken = (numBroken record) >= (length $ filter (=='.') partialDiagram)
-    notTooManyWorking = (numWorking record) >= (length $ filter (=='#') partialDiagram)
-    lastGroupNotTooLarge = if last partialDiagram == '.' then True else (length (takeWhile (=='#') $ reverse partialDiagram)) <= (head $ drop (length partialWorkingGroups) workingGroups')
+    notTooManyBroken = numBroken record >= length (filter (=='.') partialDiagram)
+    notTooManyWorking = numWorking record >= length (filter (=='#') partialDiagram)
+    lastGroupNotTooLarge = (head partialDiagram == '.') || (length (takeWhile (=='#') partialDiagram) <= (workingGroups' !! length partialWorkingGroups))
     matchesWorkingGroups = and $ zipWith (==) workingGroups' partialWorkingGroups
     workingGroups' = workingGroups record
-    partialWorkingGroups = (if last partialDiagram == '.' then id else init) $ map length $ wordsBy (=='.') partialDiagram
+    partialWorkingGroups = (if head partialDiagram == '.' then id else init) $ reverse $ map length $ wordsBy (=='.') partialDiagram
 
 parseInput :: Int -> String -> [Record]
 parseInput multiplier = map processLine . lines
@@ -86,7 +95,7 @@ toRecord multiplier [diagramOriginal, nums] = Record {
   numTotal = length diagram',
   numBroken = length diagram' - numWorking',
   numKnownBroken = length $ filter (=='.') diagram'
-} where 
+} where
     diagram' = intercalate "?" $ replicate multiplier diagramOriginal
     workingGroups' = concat $ replicate multiplier $ map parseInt $ splitOn "," nums
     numWorking' = sum workingGroups'
