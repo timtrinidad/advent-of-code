d9cef3712c6a1ac82b20c777c258ed8b886ad23e
Tim Trinidad <timtrinidad@gmail.com>
2023-12-14 21:26:18 -0800

    feat(2023-15): add haskell challenge part 1 - 3.08ms

--- haskell/2023/days/AOC15.hs
+++ haskell/2023/days/AOC15.hs
@@ -1,9 +1,15 @@
 module AOC15 (day15) where
+import Data.List.Split (splitOn)
+import Data.Char (ord)
 
 day15 = (part1, part2)
 
-part1 input = do
-  show "part1 not defined for day 15"
+part1 input = show $ sum $ map hash $ parseInput input
 
 part2 input = do
   show "part2 not defined for day 15"
+
+hash seq = foldl processChar 0 seq
+  where processChar acc char = (acc + ord char) * 17 `mod` 256
+
+parseInput input = splitOn "," input
