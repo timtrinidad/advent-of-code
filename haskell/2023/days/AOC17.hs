8d9dc324b9cd40afa57ba0cb7ceca2618e39b36a
Tim Trinidad <timtrinidad@gmail.com>
2023-12-17 18:24:18 -0800

    feat(2023-17): add haskell challenge part 2 - 27.17s

--- haskell/2023/days/AOC17.hs
+++ haskell/2023/days/AOC17.hs
@@ -3,14 +3,17 @@ module AOC17 (day17) where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Common (parseInt)
-import Algorithm.Search (dijkstra)
+import Algorithm.Search (dijkstra, aStar)
 import Data.Maybe (fromJust)
+import Data.List (group)
+import Debug.Trace (traceShow, traceShowId, trace)
 
-data Dir = North | East | South | West deriving (Show, Eq, Ord)
+data Dir = North | East | South | West | None deriving (Show, Eq, Ord)
 data Point = Pt Int Int deriving (Show, Eq, Ord)
 data PathState = PS {
   psPoint :: Point,
-  psDirHistory :: [Dir]
+  psLastDir :: Dir,
+  psConsecutiveDir :: Int
 } deriving (Show, Ord, Eq)
 
 type Mapping = Map Point Int
@@ -19,38 +22,57 @@ day17 :: (String -> String, String -> String)
 day17 = (part1, part2)
 
 part1 :: String -> String
-part1 input = show $ fst $ fromJust $ dijkstra next cost found initial
-    where
-        -- All next possible states
-        next = getNextPoints mapping
-        -- The cost for moving to the 
-        cost _ nextState = mapping Map.! psPoint nextState
-        -- Starting point is 0, 0 with no direction history
-        initial = PS { psPoint = Pt 0 0, psDirHistory = []}
-        -- We're done when we're at the bottom right corner
-        found currState = psPoint currState == Pt (width - 1) (height - 1)
-        (mapping, width, height) = parseInput input
+part1 input = show $ findPathLength mapping validateNextState validateFinalState
+  where
+    validateNextState currState nextDir =
+      psConsecutiveDir currState < 3 -- haven't had 3 consecutive moves yet
+      || psLastDir currState /= nextDir -- the next dir is different than the last dir
+    -- We're done if we're on the bottom right
+    validateFinalState currState = psPoint currState == Pt (width - 1) (height - 1) 
+    (mapping, width, height) = parseInput input
 
 part2 :: String -> String
-part2 = show . parseInput
+part2 input = show $ findPathLength mapping validateNextState validateFinalState
+  where
+    validateNextState currState nextDir
+      -- No existing moves - any direction is possible
+      | psConsecutiveDir currState == 0 = True
+      -- Last 10 moves are all equal to the proposed direction - not valid
+      | psConsecutiveDir currState == 10 && psLastDir currState == nextDir = False
+      -- Less than 4 consecutive moves - needs to be the same as last
+      | psConsecutiveDir currState < 4 && psLastDir currState /= nextDir = False
+      | otherwise = True
+    -- We'de done if we're on the bottom right AND we've had at least 4 consecutive moves
+    validateFinalState currState = psPoint currState == Pt (width - 1) (height - 1) && psConsecutiveDir currState >= 4
+    (mapping, width, height) = parseInput input
+
+-- Find the length of the path from the top left to the bottom right based on the challenge rules
+findPathLength :: Mapping -> (PathState -> Dir -> Bool) -> (PathState -> Bool) -> Int
+findPathLength mapping validateNextState found = fst $ fromJust $ dijkstra next cost found initial
+    where
+        -- All next possible states
+        next = getNextPoints mapping validateNextState
+        -- The cost for moving to the 
+        cost _ nextState = mapping Map.! psPoint nextState
+        -- heuristic (PS (Pt x y) _) = (width - x) + (height - y)
+        -- Starting point is 0, 0 with no direction history
+        initial = PS { psPoint = Pt 0 0, psConsecutiveDir = 0, psLastDir = None}
 
 -- Given a current path state, determine a list of next possible path states
-getNextPoints :: Mapping -> PathState -> [PathState]
-getNextPoints mapping currState = filter inMapping $ map toState $ filter validDir [North, South, East, West]
+getNextPoints :: Mapping -> (PathState -> Dir -> Bool) -> PathState -> [PathState]
+getNextPoints mapping validateNextState currState = filter inMapping $ map toState  $ filter validDir [North, South, East, West]
   where
     -- Make sure generated point is actually within the map
     inMapping point = Map.member (psPoint point) mapping
     toState dir = PS {
       -- The new point given the current point and a direction
       psPoint = transformPoint (psPoint currState) $ dirDelta dir,
-      -- Keep track of the last 3 directions
-      psDirHistory = take 3 (dir:dirHistory)
+      psLastDir = dir,
+      psConsecutiveDir = 1 + (if dir == psLastDir currState then psConsecutiveDir currState else 0)
     }
-    -- Is valid if we're not going backwards and we're not more than 3 consecutive in one direction
-    validDir dir = isNotBackwards dir && isNotFourthConsecutive dir
-    isNotBackwards dir = null dirHistory || not (isOpposite dir $ head dirHistory)
-    isNotFourthConsecutive dir = length dirHistory < 3 || any (/=dir) dirHistory 
-    dirHistory = psDirHistory currState -- Last 3 directions for the current state
+    -- Is valid if we're not going backwards and the given validation passes
+    validDir dir = isNotBackwards dir && validateNextState currState dir
+    isNotBackwards dir = not (isOpposite dir $ psLastDir currState)
 
 -- Given two points, determine if they are opposites
 isOpposite :: Dir -> Dir -> Bool
