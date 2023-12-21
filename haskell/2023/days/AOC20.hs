136d0f5e4a356dd3d77e8e977cc86c8af924f5d5
Tim Trinidad <timtrinidad@gmail.com>
2023-12-20 20:27:31 -0800

    feat(2023-20): add haskell challenge part 2 - n/a ms

--- haskell/2023/days/AOC20.hs
+++ haskell/2023/days/AOC20.hs
@@ -3,7 +3,7 @@
 module AOC20 (day20) where
 import Data.List.Split (wordsBy, splitOn)
 import Text.Show.Pretty (ppShow)
-import Data.List (intercalate, isInfixOf)
+import Data.List ( intercalate, isInfixOf, foldl' )
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Debug.Trace
@@ -12,64 +12,93 @@ day20 :: (String -> String, String -> String)
 day20 = (part1, part2)
 
 type Config = Map String PulseModule
-type PulseCounts = Map Bool Int
-data PMType = Broadcaster | Conjunction | FlipFlop | None deriving (Show)
+type PulseCounts = Map PMState Int
+data PMState = HighPulse | LowPulse | FFOn | FFOff deriving (Show, Eq, Ord)
+data PMType = Broadcaster | Conjunction | FlipFlop | None deriving (Show, Eq)
+data PulseAction = PA String PMState String deriving (Show)
 data PulseModule = PM {
     pmType :: PMType,
     pmName :: String,
     pmDests :: [String],
-    pmState :: Map String Bool
+    pmState :: Map String PMState
 }
-data PulseAction = PA String String Bool deriving (Show)
 instance Show PulseModule where
   show (PM a b c d) = "{" ++ show a ++ " " ++ b ++ " -> " ++ intercalate "," c ++ " | " ++ show (Map.assocs d) ++ "}"
 
 part1 :: String -> String
-part1 input = show $ product $ Map.elems $ pushButton 1000 config Map.empty
-    where config = parseInput input
+part1 input = show $ product -- multiply counts
+    $ Map.elems $ snd -- Get counts
+    $ foldl pressButton (config, Map.empty) [1..1000] -- Press the button 1000 times
+    where
+        config = parseInput input
+        pressButton (config', counts') _ = processPulses [PA "button" LowPulse "broadcaster"] config' counts'
 
 part2 :: String -> String
-part2 = show . parseInput
-
-pushButton 0 _ counts = counts
-pushButton num config counts = pushButton (num-1) config' counts'
+part2 input = outputDiagram config
     where
-        (config', counts') = processPulses config [PA "button" "broadcaster" False] counts
+        config = parseInput input
 
-processPulses :: Num a => Map String PulseModule -> [PulseAction] -> Map Bool a -> (Map String PulseModule, Map Bool a)
-processPulses config [] pulseCounts = (config, pulseCounts)
-processPulses config (currAction@(PA srcName destName isHighPulse):restActions) pulseCounts = processPulses config' restActions' pulseCounts'
+-- Output a graph diagram to be rendered by mermaid.live
+outputDiagram :: Config -> String
+outputDiagram config = unlines $ concatMap split $ Map.elems config
     where
-        pulseCounts' = Map.insertWith (+) isHighPulse 1 pulseCounts
+        split pulseModule = map (\x -> "\"" ++ pmTypeName pulseModule ++ "\" --> \"" ++ (if Map.member x config then pmTypeName (config Map.! x) else x) ++ "\"") $ pmDests pulseModule
+        pmTypeName pulseModule' = (if pmType pulseModule' == FlipFlop then "%" else "&") ++ pmName pulseModule'
+
+-- Recursively process pulses until none are left in the queue
+processPulses :: [PulseAction] -> Config -> PulseCounts -> (Config, PulseCounts )
+-- Base case - no more pulses
+processPulses [] config pulseCounts = (config, pulseCounts)
+-- Recurse, incrementing pulse counts with updated state
+processPulses ((PA srcName pulseType destName):restActions) config pulseCounts = processPulses restActions' config' pulseCounts'
+    where
+        -- Increment counts
+        pulseCounts' = Map.insertWith (+) pulseType 1 pulseCounts
+        -- Append new actions to list
         restActions' = restActions ++ newActions
-        (config', newActions) = generateActions config isHighPulse srcName destModule
+        -- Determine next actions and updated state based on module type
+        (config', newActions) = generateActions config pulseType srcName destModule
         destModule = Map.findWithDefault defaultModule destName config
         defaultModule = PM { pmName = destName, pmType = None, pmDests = [], pmState = Map.empty }
 
-generateActions config isHighPulse _ pulseModule@PM { pmType=FlipFlop } = (config', newActions isHighPulse currOn)
+
+-- Update the state and generate new pulses depending on the module type
+generateActions :: Config -> PMState -> String -> PulseModule -> (Config, [PulseAction])
+generateActions config pulseType _ pulseModule@PM { pmType=FlipFlop } = (config', newActions pulseType currOn)
     where
-        config' = Map.insert currModuleName (pulseModule { pmState = Map.fromList [("on", nextOn isHighPulse currOn)]}) config
-        newActions True _ = []
-        newActions False True = map (\pmDest -> PA currModuleName pmDest False) $ pmDests pulseModule
-        newActions False False = map (\pmDest -> PA currModuleName pmDest True) $ pmDests pulseModule
-        nextOn True = id
-        nextOn False = not
+        -- Update state
+        config' = Map.insert currModuleName (pulseModule { pmState = Map.fromList [("on", nextOn pulseType)]}) config
+        -- If high pulse, keep current state and don't send new pulses
+        -- If low pulse, alternate state and send pulses
+        nextOn HighPulse = currOn
+        nextOn LowPulse = if currOn == FFOn then FFOff else FFOn
+        newActions HighPulse _ = []
+        newActions LowPulse FFOn = map (PA currModuleName LowPulse) $ pmDests pulseModule
+        newActions LowPulse FFOff = map (PA currModuleName HighPulse) $ pmDests pulseModule
+        -- Whether or not this switch is currently on
         currOn  = pmState pulseModule Map.! "on"
         currModuleName = pmName pulseModule
-generateActions config isHighPulse srcName pulseModule@PM { pmType=Conjunction } = (config', newActions)
+-- Conjunction - update state before determining pulse and send to destinations
+generateActions config pulseType srcName pulseModule@PM { pmType=Conjunction } = (config', newActions)
     where
+        -- Update state
         config' = Map.insert currModuleName (pulseModule { pmState = incomingPulseHist }) config
-        newActions = map (\pmDest -> PA currModuleName pmDest pulseToSend) $ pmDests pulseModule
-        pulseToSend = not $ and $ Map.elems incomingPulseHist
-        incomingPulseHist = Map.insert srcName isHighPulse $ pmState pulseModule
+        -- Send pulse to all destinations
+        newActions = map (PA currModuleName pulseToSend) $ pmDests pulseModule
+        -- Low if all True, High otherwise
+        pulseToSend = if all (==HighPulse) $ Map.elems incomingPulseHist then LowPulse else HighPulse
+        -- Existing state keyed by all incoming connections
+        incomingPulseHist = Map.insert srcName pulseType $ pmState pulseModule
         currModuleName = pmName pulseModule
-generateActions config isHighPulse _ pulseModule@PM { pmType=Broadcaster } = (config, newActions)
+-- Broadcaster - resend pulse to destinations
+generateActions config pulseType _ pulseModule@PM { pmType=Broadcaster } = (config, newActions)
     where
-        newActions = map (\pmDest -> PA "broadcaster" pmDest isHighPulse) $ pmDests pulseModule
+        newActions = map (PA "broadcaster" pulseType) $ pmDests pulseModule
+-- All other types - don't update config and don't generate new actions
 generateActions config _ _ _ = (config, [])
 
-parseInput :: String -> Map String PulseModule
-parseInput input = Map.fromList $ map (parseLine allLines . wordsBy (`elem` " ->,")) $ allLines
+parseInput :: String -> Config
+parseInput input = Map.fromList $ map (parseLine allLines . wordsBy (`elem` " ->,")) allLines
     where allLines = lines input
 
 parseLine :: [String] -> [String] -> (String, PulseModule)
@@ -82,8 +111,8 @@ parseLine allLines (moduleKey:destinations) =  (moduleName, pm)
             pmDests = destinations,
             pmState = initState moduleType
         }
-        initState FlipFlop = Map.fromList [("on", False)]
-        initState Conjunction = Map.fromList $ map (\x -> (x, False)) $ findInputs allLines moduleName
+        initState FlipFlop = Map.fromList [("on", FFOff)]
+        initState Conjunction = Map.fromList $ map (\x -> (x, LowPulse)) $ findInputs allLines moduleName
         initState _ = Map.empty
 
 parseModuleKey :: String -> (PMType, String)
