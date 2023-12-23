50ccfb3085629b6c0d66e9ebac6dbdcaa3eef441
Tim Trinidad <timtrinidad@gmail.com>
2023-12-23 15:06:17 -0800

    feat(2023-23): add haskell challenge part 2 - 12.98s

--- haskell/2023/days/AOC23.hs
+++ haskell/2023/days/AOC23.hs
@@ -1,13 +1,9 @@
-{-# LANGUAGE RecordWildCards #-}
 module AOC23 (day23) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Common (parseInt)
-import Data.List (sort, maximumBy)
-import Debug.Trace
 import Text.Show.Pretty (ppShow)
 import Data.Ord (comparing)
 
@@ -19,14 +15,72 @@ day23 :: (String -> String, String -> String)
 day23 = (part1, part2)
 
 part1 :: String -> String
-part1 input = show $ length path
-    -- renderMap mapping path width height
+part1 input = show $ findLongestPath mapping start end
     where
-        path = traverseMapping mapping end start
-        start = Pt 1 1
+        start = Pt 1 0
         end = Pt (width - 2) (height - 1)
         (mapping, width, height) = parseInput input
 
+part2 :: String -> String
+part2 input = show $ findLongestPath mapping' start end
+    where
+        start = Pt 1 0
+        end = Pt (width - 2) (height - 1)
+        mapping' = removeSlopes mapping
+        (mapping, width, height) = parseInput input
+
+-- Find the longest path between the start and end point
+-- by reducing the full map into nodes representing the intersections
+-- and having the cost be the distance between each intersection
+findLongestPath :: Mapping -> Point -> Point -> Int
+findLongestPath mapping start end = maximum $ map sum $ traverseIntersections start end Set.empty [] intersectionDists
+    where
+        intersectionDists = Map.fromList $ map handleIntersection intersections
+        handleIntersection intersection = (intersection, findIntersectionNeighbor mapping intersections 0 intersection)
+        intersections = findIntersections mapping ++ [start, end]
+
+
+-- DFS - Recursively find the distance from a given intersection to all its neighboring intersections
+findIntersectionNeighbor :: Mapping -> [Point] -> Int -> Point -> [(Point, Int)]
+findIntersectionNeighbor mapping intersections dist currPt 
+    -- We've reached another intersection - return the neighboring intersection point and its distance
+    | dist > 0 && currPt `elem` intersections = [(currPt, dist)]
+    -- Recurse, incrementing distance and exploring all neighboars (should only branch on first call
+    -- since it should return once it hits another branch/neighbor
+    | otherwise = concatMap (findIntersectionNeighbor mapping' intersections (dist + 1)) $ findNeighbors mapping' currPt
+    where
+        -- Alternative to "Seen" - mark the previously traversed points as a wall
+        mapping' = Map.insert currPt '#' mapping
+
+-- DFS to traverse all intersections to find all possible path distances.
+-- Returns an array of paths represented by the distances between intersections in the path.
+traverseIntersections :: Point -> Point -> Set Point -> [Int] -> Map Point [(Point, Int)] -> [[Int]]
+traverseIntersections curr end seen path intersections
+    -- Reached the end - return the current path (distances)
+    | curr == end = [path]
+    -- Already seen this - quit immediately
+    | Set.member curr seen = []
+    -- Recurse, exploring each possible intersectino neighbor
+    | otherwise = concatMap (\(dest, dist) -> traverseIntersections dest end seen' (path ++ [dist]) intersections) destinations
+    where
+        -- All possible other intersections from the current intersection
+        destinations = intersections Map.! curr
+        seen' = Set.insert curr seen
+
+-- Find a list of all points that are intersections (more than 2 neighboring points)
+findIntersections :: Mapping -> [Point]
+findIntersections mapping = Map.keys $ Map.filterWithKey isIntersection mapping
+    where
+        -- wall - definitely not an intersection
+        isIntersection _ '#' = False
+        isIntersection pt _ = length (findNeighbors mapping pt) > 2
+
+-- For part 2 - convert all slopes to regular points
+removeSlopes :: Mapping -> Mapping
+removeSlopes = Map.map (\x -> if x == '#' then '#' else '.')
+
+-- For debugging - render a map with an O for the given path
+renderMap :: Mapping -> [Point] -> Int -> Int -> String
 renderMap mapping path width height = unlines $ map renderLine [0..height-1]
     where
         renderLine y = map (renderChar y) [0..width-1]
@@ -34,34 +88,22 @@ renderMap mapping path width height = unlines $ map renderLine [0..height-1]
             | Pt x y `elem` path = 'O'
             | otherwise = mapping Map.! Pt x y
 
-traverseMapping mapping end currPt
-    | currPt == end = [currPt]
-    | null neighbors = []
-    | null pathOptions = []
-    | otherwise = currPt : maximumBy (comparing length) pathOptions
+-- Given a mapping and a point, find all valid neighbors accounting
+-- for slopes (being on one or trying to climb one), map edges, and walls
+findNeighbors :: Mapping -> Point -> [Point]
+findNeighbors mapping pt = map snd neighbors -- strip out relative direction
     where
-        pathOptions = filter (not . null) $ map traverseNeighbor neighbors
-        traverseNeighbor = traverseMapping mapping' end
-        neighbors = findNeighbors mapping' currPt
-        mapping' = Map.insert currPt '#' mapping
-
-
-findNeighbors mapping pt = map snd neighbors
-    where
-        neighbors = filter isValid $ map dirToPt $ validDirs char
-        isValid (dir, neighbor) = canTraverse (dir, Map.findWithDefault '#' neighbor mapping)  
-        char = mapping Map.! pt
+        -- All valid neighbors
+        neighbors = filter isValid $ map dirToPt $ validDirs currentPointType
+        -- Is valid if we can move into the neighboring spot based on canTraverse
+        isValid (dir, neighbor) = canTraverse (dir, Map.findWithDefault '#' neighbor mapping)
+        currentPointType = mapping Map.! pt
+        -- A neighbor and its relative direction to the current point
         dirToPt dir = (dir, transformPoint pt $ dirDelta dir)
 
--- getTopologicalOrder _ [] _ order = order
--- getTopologicalOrder mapping (currPt:restPts) num order
---     | Map.member currPt order = getTopologicalOrder mapping restPts num order
---     | otherwise = getTopologicalOrder mapping restPts' num' order'
---     where
---         num' = num + 1
---         restPts' = restPts ++ findNeighbors mapping currPt
---         order' = traceShow (currPt, num) $ Map.insert currPt num order
-
+-- Given a source direction and a new point, determine whether or not
+-- we can traverse that point.
+canTraverse :: (Dir, Char) -> Bool
 canTraverse (_, '#') = False
 canTraverse (North, 'v') = False
 canTraverse (South, '^') = False
@@ -69,14 +111,13 @@ canTraverse (East, '<') = False
 canTraverse (West, '>') = False
 canTraverse _ = True
 
+-- If we're at a point, determine valid directions (not accounting for walls)
+validDirs :: Char -> [Dir]
 validDirs '>' = [East]
 validDirs '<' = [West]
 validDirs 'v' = [South]
 validDirs '^' = [North]
 validDirs _ = [North, East, South, West]
-
-part2 :: String -> String
-part2 = show . parseInput
 
 -- Convert a direction to an x y delta
 dirDelta :: Dir -> (Int, Int)
