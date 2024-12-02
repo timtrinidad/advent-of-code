674675791e369d15761b870e7c3f62c8be0bb559
Tim Trinidad <tim@characterstrong.com>
2024-12-01 22:45:19 -0800

    feat(2024-02): add js challenge part 2 - 0.078s

--- js/2024/aoc2402.js
+++ js/2024/aoc2402.js
@@ -11,16 +11,29 @@ function parse(inputs) {
 }
 
 function part1(parsed) {
-    return parsed.map(x => {
-        const increasing = x.every((curr, i, arr) => i === 0 || curr > arr[i-1]);
-        const decreasing = x.every((curr, i, arr) => i === 0 || curr < arr[i-1]);
-        const noGaps = x.every((curr, i, arr) => i === 0 || Math.abs(curr - arr[i-1]) <= 3);
-        return (increasing || decreasing) && noGaps;
-    }).filter(x => x).length;
+    return parsed.map(isSafe).filter(x => x).length;
 }
 
 function part2(parsed) {
-    return 0;
+    return parsed.map(x => {
+        if(isSafe(x)) {
+            return true;
+        }
+        for (let i = 0; i < x.length; i++) {
+            if(isSafe(x.filter((_, idx) => i !== idx))) {
+                return true;
+            }
+        }
+        return false;
+    }).filter(x => x).length;
+}
+
+function isSafe(x) {
+    console.log(x);
+    const increasing = x.every((curr, i, arr) => i === 0 || curr > arr[i-1]);
+    const decreasing = x.every((curr, i, arr) => i === 0 || curr < arr[i-1]);
+    const noGaps = x.every((curr, i, arr) => i === 0 || Math.abs(curr - arr[i-1]) <= 3);
+    return (increasing || decreasing) && noGaps;
 }
 
 run(__filename, solve);
