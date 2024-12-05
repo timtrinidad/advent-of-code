c9d71bfad652b19ffc7870aa8d162b772dcff8e3
Tim Trinidad <tim@characterstrong.com>
2024-12-05 15:21:28 -0800

    feat(2024-05): add js challenge part 2 - 0.004s

--- js/2024/aoc2405.js
+++ js/2024/aoc2405.js
@@ -8,35 +8,68 @@ async function solve(inputs, partNum, isSample) {
 
 function parse(inputs) {
     const inputStr = inputs.join("\n");
-    let [rules, pages] = inputStr.split("\n\n").map(x => x.split("\n"));
-    rules = new Set(rules);
-    pages = pages.map(x => x.split(",").map(y => parseInt(y, 10)));
+    let [rules, lines] = inputStr.split("\n\n").map(x => x.split("\n"));
+    lines = lines.map(x => x.split(",").map(y => parseInt(y, 10)));
     
-    return [rules, pages]
+    return [rules, lines]
 }
 
-function part1([rules, pages]) {
-    // Go through each page line
-    const validPages = pages.filter(pageLine => {
-        for(let i = 0; i < pageLine.length - 1; i++) {
-            for(let j = i + 1; j < pageLine.length; j++) {
-                // Filter out any line that contains a pair whose opposite
-                // order appears in the rules
-                if(rules.has(`${pageLine[j]}|${pageLine[i]}`)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    });
+function part1([rules, lines]) {
+    const validLines = filterLines(lines, new Set(rules), true)
 
     // Get the mids and sum them
-    const mids = validPages.map(x => x[Math.floor(x.length/2)]);
+    const mids = validLines.map(x => x[Math.floor(x.length/2)]);
     return mids.reduce((prev, next) => prev + next);
 }
 
-function part2(parsed) {
-    return 0;
+function part2([rules, lines]) {
+    const ruleSet = new Set(rules);
+    const invalidLines = filterLines(lines, ruleSet, false);
+    const correctedLines = invalidLines.map(line => fixLine(line, [], ruleSet));
+    
+    const mids = correctedLines.map(x => x[Math.floor(x.length/2)]);
+    return mids.reduce((prev, next) => prev + next);
 }
 
-run(__filename, solve);
+/**
+ * Get only lines that are valid based on the rules
+ */
+function filterLines(lines, rules, returnValid) {
+    return lines.filter(line => returnValid ? isLineValid(line, rules) : !isLineValid(line, rules));
+}
+
+function isLineValid(line, rules) {
+    for(let i = 0; i < line.length - 1; i++) {
+        for(let j = i + 1; j < line.length; j++) {
+            // Filter out any line that contains a pair whose opposite
+            // order appears in the rules
+            if(rules.has(`${line[j]}|${line[i]}`)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+/**
+ * Reorder the numbers on a line based on the given rules
+ */
+function fixLine(remainingNums, existingLine, rules) {
+    if(!remainingNums.length) {
+        return existingLine;
+    }
+
+    // Look for which number doesn't appear in the rules on the right for any of the remaining numbers on the left
+    // That's our next number
+    const firstNum = remainingNums.find(x => {
+        return remainingNums.every(y => {
+            return x === y || !rules.has(`${y}|${x}`);
+        });
+    });
+
+    existingLine.push(firstNum);
+    // Recurse to find the remaining numbers
+    return fixLine(remainingNums.filter(x => x !== firstNum), existingLine, rules);
+}
+
+run(__filename, solve, {skipTests: false});
