e85f22504b5164cd3c7e73f2d93e9badb8cbd7c1
Tim Trinidad <timtrinidad@gmail.com>
2024-12-07 08:00:18 -0800

    feat(2024-07): improve js challenge part 2 - 2.040s

--- js/2024/aoc2407.js
+++ js/2024/aoc2407.js
@@ -9,71 +9,48 @@ async function solve(inputs, partNum, isSample) {
 function parse(inputs) {
     return inputs.map(x => {
         // Parse out all numbers, store first one as the "total" and the rest as values
-        const [res, ...vals] = [...x.matchAll(/(\d+)/g)].map(y => parseInt(y[0], 10));
+        const [res, ...vals] =
+            [...x.matchAll(/(\d+)/g)]
+            .map(y => parseInt(y[0], 10));
         return {res, vals}
     });
 }
 
-function part1(parsed, possibleOps = ['+', '*']) {
+function part1(parsed, possibleOps = [multiply, add]) {
     let sum = 0;
 
-    for (const line of parsed) {
+    for (const {res, vals} of parsed) {
         // If any given line can be made into an equation,
         // add its total to the sum
-        if(isValid(line, possibleOps)) {
-            sum += line.res;
+        if(isValid(vals.slice(1), vals[0], res, possibleOps)) {
+            sum += res;
         }
     }
     return sum;
 }
 
 function part2(parsed) {
-    return part1(parsed, ['+', '*', '|'])
+    return part1(parsed, [multiply, add, combine])
 }
 
 
 /**
- * Determine, if based on the fully expanded set of possible equations,
- * if the result can be accomplished from the given values.
+ * Determine, using DFS,if the result can be accomplished from the given values.
  */
-function isValid({res, vals}, possibleOps) {
-    // No need to expand the first value into all possible options - it will
-    // always just be returned (specifically be added to the initial value of 0)
-    const equation = [{op: '+', val: vals[0]}];
-
-    // Get the full set of possible equations - permutations of equations combined with values
-    const equations = expandOps(vals.slice(1), equation, possibleOps);
-
-    // Use those operations to get an output number from the values
-    // and return true if any of them match our result for the line
-    for (const equation of equations) {
-        const total = equation.reduce((prev, {val, op}) => {
-            switch(op) {
-                case '+': return prev + val;
-                case '*': return prev * val;
-                case '|': return parseInt(`${prev}${val}`, 10);
-            }
-            return op === '+' ? prev + val : prev * val;
-        }, 0);
-        if(total === res) {
-            return true;
-        }
+function isValid(remainingValues, actual, expected, possibleOps) {
+    if(!remainingValues.length) {
+        return expected === actual;
     }
-    return false;
+    return possibleOps.some(op => isValid(
+        remainingValues.slice(1), // Recurse without the number we just used
+        op(actual, remainingValues[0]), // Run the operation and update our accumulator
+        expected, // Just pass along the exit condition
+        possibleOps // Pass along our possible operations
+    ));
 }
 
-/**
- * Recursively combine each value with one of the various operators
- * and return the full set of permutations
- */
-function expandOps(vals, equation = [], possibleOps) {
-    if(!vals.length) {
-        return [equation];
-    }
-    const currVal = vals[0];
-    return possibleOps.flatMap(op => {
-        return expandOps(vals.slice(1), [...equation, {val: currVal, op}], possibleOps)
-    });
-}
+const multiply = (a, b) => a*b;
+const add = (a, b) => a+b;
+const combine = (a, b) => parseInt(`${a}${b}`, 10);
 
 run(__filename, solve);
