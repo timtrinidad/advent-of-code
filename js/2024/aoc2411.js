1ee8b9ec5ad3a121f803253b6da33fe4ae9ec288
Tim Trinidad <tim@characterstrong.com>
2024-12-11 00:08:04 -0800

    feat(2024-11): add js challenge part 2 - 0.029s

--- js/2024/aoc2411.js
+++ js/2024/aoc2411.js
@@ -2,7 +2,11 @@ const { run } = require('aoc-copilot');
 
 async function solve(inputs, partNum, isSample) {
     if(isSample) {
-        inputs = ['125 17']
+        if(partNum === 2) {
+            return 0;
+        }
+
+        inputs = ['125 17'];
     }
 
     const parsed = parse(inputs);
@@ -12,31 +16,44 @@ async function solve(inputs, partNum, isSample) {
 }
 
 function parse(inputs) {
-    return inputs[0].split(' ').map(x => parseInt(x, 10));
+    return new Map(inputs[0].split(' ').map(x => [parseInt(x, 10), 1]));
 }
 
-function part1(parsed) {
+function part1(parsed, numIterations = 25) {
     let curr = parsed;
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < numIterations; i++) {
         curr = blink(curr);
     }
-    return curr.length;
+    return [...curr.values()].reduce((prev, curr) => prev + curr)
 }
 
 function part2(parsed) {
-    return 0;
+    return part1(parsed, 75);
+
 }
 
+/**
+ * Process all stones during a blink, making sure to only process each unique value once
+ * (and keep track of the number of repeat values each time)
+ */
 function blink(stones) {
-    return stones.flatMap(stone => {
-        const stoneStr = stone.toString();
-        if(stone === 0) { return [1]; };
-        if(stoneStr.length % 2 === 0) { return [
-            parseInt(stoneStr.substring(0, stoneStr.length/2), 10), 
-            parseInt(stoneStr.substring(stoneStr.length/2), 10)
-        ]; }
-        return stone * 2024;
-    })
+    const map = new Map();
+    for (const [val, numVal] of stones) {
+        for (const newVal of processStone(val)) {
+            map.set(newVal, (map.get(newVal) || 0) + numVal)
+        }
+    }
+    return map;   
+}
+
+function processStone(stone) {
+    const stoneStr = stone.toString();
+    if(stone === 0) { return [1]; };
+    if(stoneStr.length % 2 === 0) { return [
+        parseInt(stoneStr.substring(0, stoneStr.length/2), 10), 
+        parseInt(stoneStr.substring(stoneStr.length/2), 10)
+    ]; }
+    return [stone * 2024];
 }
 
 run(__filename, solve);
