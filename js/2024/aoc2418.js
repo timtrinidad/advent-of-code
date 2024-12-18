1cbb550e3ed79885b282233aa39f42d9e6029d9f
Tim Trinidad <timtrinidad@gmail.com>
2024-12-17 22:24:23 -0800

    feat(2024-18): add js challenge part 2 - 0.279s

--- js/2024/aoc2418.js
+++ js/2024/aoc2418.js
@@ -7,7 +7,7 @@ const DIRS = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 async function solve(inputs, partNum, isSample) {
     const parsed = parse(inputs);
 
-    return partNum === 1 ? part1(parsed, isSample) : part2(parsed);
+    return partNum === 1 ? part1(parsed, isSample) : part2(parsed, isSample);
 }
 
 function parse(inputs) {
@@ -41,8 +41,41 @@ function part1(parsed, isSample) {
     return graph.path(mapKey(0, 0), mapKey(width - 1, width - 1)).length - 1;
 }
 
-function part2(parsed) {
-    return 0;
+function part2(parsed, isSample) {
+    const corrupted = parsed.map(x => mapKey(...x));
+
+    const width = isSample ? 6 + 1 : 70 + 1;
+    const graph = new Graph();
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < width; y++) {
+            const neighbors = [];
+            for (const [dx, dy] of DIRS) {
+                const x1 = x + dx;
+                const y1 = y + dy;
+                if(x1 < 0 || x1 >= width || y1 < 0 || y1 >= width) {
+                    continue;
+                }
+                neighbors[mapKey(x1, y1)] = 1;
+            }
+            graph.addNode(mapKey(x, y), neighbors);
+        }
+    }
+
+    const avoid = [];
+    let start = 0;
+    let end = corrupted.length - 1;
+    while (start < end) {
+        const mid = Math.floor((start + end) / 2);
+        const hasPath = graph.path(mapKey(0, 0), mapKey(width - 1, width - 1), {
+            avoid: corrupted.slice(0, mid)
+        });
+        if(hasPath) {
+            start = mid + 1
+        } else {
+            end = mid;
+        }
+    }
+    return corrupted[start-1];
 }
 
-run(__filename, solve, {skipTests: false});
+run(__filename, solve);
