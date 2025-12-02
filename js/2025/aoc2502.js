b838e10ca8f11953996987ac5a2a619ba96c1182
Tim Trinidad <timtrinidad@gmail.com>
2025-12-01 23:19:00 -0800

    feat(2025-02): add js challenge part 2 - 0.033s

--- js/2025/aoc2502.js
+++ js/2025/aoc2502.js
@@ -41,7 +41,32 @@ function part1(parsed) {
 }
 
 function part2(parsed) {
-    return 0;
+    // parsed = [['95', '115']]
+    const invalidNums = new Set();
+    parsed.forEach(([left, right]) => {
+        const leftLength = 1
+        const rightLength = Math.ceil(right.length/2);
+        const min = leftLength === 0 ? 0 : left.substring(0, leftLength);
+        const max = right.substring(0, rightLength);
+        const leftInt = parseInt(left, 10);
+        const rightInt = parseInt(right, 10);
+        const minInt = parseInt(min, 10);
+        const maxInt = parseInt(max, 10);
+
+        for (let i = 0; i <= maxInt; i++) {
+            for (let times = 1; times <= 10; times++) {
+                const toTest = parseInt(i.toString().repeat(times), 10);
+                if(toTest > rightInt) {
+                    break;
+                }
+                if(toTest >= leftInt && toTest <= rightInt) {
+                    invalidNums.add(toTest);
+                }
+            }
+        }
+
+    })
+    return Array.from(invalidNums).reduce((prev, curr) => prev + curr);
 }
 
 run(__filename, solve);
