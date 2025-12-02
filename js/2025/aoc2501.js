714e7d19f497743278a89b83b1df025fdcc00e5c
Tim Trinidad <timtrinidad@gmail.com>
2025-12-01 22:14:08 -0800

    feat(2025-01): add js challenge part 1 - 0.000s

---
+++ js/2025/aoc2501.js
@@ -0,0 +1,30 @@
+const { run } = require('aoc-copilot');
+const { mapKey, parseMapKey, DIRS4, DIRS8 } = require('../utils');
+
+async function solve(inputs, partNum, isSample) {
+    const parsed = parse(inputs);
+    
+    return partNum === 1 ? part1(parsed) : part2(parsed);
+}
+
+function parse(inputs) {
+    return inputs;
+}
+
+function part1(parsed) {
+    let i = 50;
+    let numZeros = 0;
+    parsed.map(x => (x[0] === "L" ? -1 : 1 ) * parseInt(x.substring(1), 10)).forEach(x => {
+        i = (i + 100 + x) % 100;
+        if (i === 0) {
+            numZeros++;
+        }
+    });
+    return numZeros;
+}
+
+function part2(parsed) {
+    return 0;
+}
+
+run(__filename, solve);
