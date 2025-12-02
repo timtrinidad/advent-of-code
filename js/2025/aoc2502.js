d2413e5021c5648c24b943a07e8fbbeff40e1f43
Tim Trinidad <timtrinidad@gmail.com>
2025-12-01 23:06:41 -0800

    feat(2025-02): add js challenge part 1 - 0.005s

---
+++ js/2025/aoc2502.js
@@ -0,0 +1,47 @@
+const { run } = require('aoc-copilot');
+const { mapKey, parseMapKey, DIRS4, DIRS8 } = require('../utils');
+
+async function solve(inputs, partNum, isSample) {
+    const parsed = parse(inputs);
+    
+    return partNum === 1 ? part1(parsed) : part2(parsed);
+}
+
+function parse(inputs) {
+    return inputs
+        .join('')
+        .replace('\n', '')
+        .split(',')
+        .map(x => x
+            .split('-')
+        )
+}
+
+function part1(parsed) {
+    const invalidNums = [];
+    parsed.forEach(([left, right]) => {
+        const leftLength = Math.floor(left.length/2);
+        const rightLength = Math.ceil(right.length/2);
+        const min = leftLength === 0 ? 0 : left.substring(0, leftLength);
+        const max = right.substring(0, rightLength);
+        const leftInt = parseInt(left, 10);
+        const rightInt = parseInt(right, 10);
+        const minInt = parseInt(min, 10);
+        const maxInt = parseInt(max, 10);
+
+        for (let i = minInt; i <= maxInt; i++) {
+            const toTest = parseInt(`${i}${i}`, 10);
+            if(toTest >= leftInt && toTest <= rightInt) {
+                invalidNums.push(toTest);
+            }
+        }
+
+    })
+    return invalidNums.reduce((prev, curr) => prev + curr);
+}
+
+function part2(parsed) {
+    return 0;
+}
+
+run(__filename, solve);
