54b2b10ec1febf84d4838a8a276d3b8dcd573ebe
Tim Trinidad <timtrinidad@gmail.com>
2025-12-02 23:11:01 -0800

    feat(2025-03): add js challenge part 2 - 1.295s

--- js/2025/aoc2503.js
+++ js/2025/aoc2503.js
@@ -28,7 +28,48 @@ function part1(parsed) {
 }
 
 function part2(parsed) {
-  return 0;
+  const vals = parsed.map((line, lineNum) => {
+    console.log(lineNum);
+    return findMax(line);
+  });
+  return vals.reduce((acc, curr) => acc + curr);
+}
+
+function findMax(nums) {
+  const queue = [[nums, 0]];
+  let currMax = 0;
+
+  // BFS through each list of numbers
+  const seen = new Map();
+  while (queue.length) {
+    const [[curr, ...rest], max] = queue.pop();
+
+    // Exit early if a greater number with the same length was already found
+    const lenMax = max.toString().length;
+    if (seen.has(lenMax) && seen.get(lenMax) > max) {
+      continue;
+    }
+    seen.set(lenMax, max);
+
+    // Terminal condition
+    if (max >= 100_000_000_000) {
+      // Keep track of current max 12 digit number
+      if (max > currMax) {
+        currMax = max;
+      }
+      continue;
+    }
+
+    if (curr) {
+      queue.push([rest, max * 10 + curr]);
+    }
+
+    // Don't skip if we'll have less than the required number of digits left
+    if (rest.length + max.toString().length >= 12) {
+      queue.push([rest, max]);
+    }
+  }
+  return currMax;
 }
 
 run(__filename, solve);
