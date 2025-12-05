b577ff98bf0d3f60bad496edffd2a1c41d443b27
Tim Trinidad <timtrinidad@gmail.com>
2025-12-04 22:38:22 -0800

    feat(2025-05): add js challenge part 2 - 0.003s

--- js/2025/aoc2505.js
+++ js/2025/aoc2505.js
@@ -26,8 +26,60 @@ function part1({ ranges, ids }) {
   return freshIds.length;
 }
 
-function part2(parsed) {
-  return 0;
+function part2({ ranges }) {
+  let numRanges = ranges.length;
+  while (true) {
+    ranges = combineRanges(ranges);
+    if (ranges.length === numRanges) {
+      break;
+    }
+    numRanges = ranges.length;
+  }
+
+  return ranges.reduce((acc, [min, max]) => acc + max - min + 1, 0);
+}
+
+function combineRanges(ranges) {
+  return ranges
+    .reduce((actualRanges, [min, max]) => {
+      const newSet = [];
+      let needsToBeAdded = true;
+      for (const [actualMin, actualMax] of actualRanges) {
+        // Do nothing if falls within an existing range
+        if (min >= actualMin && max <= actualMax) {
+          return actualRanges;
+        }
+        // Min is outside range but max is within range - extend min
+        if (min <= actualMin && max >= actualMin && max <= actualMax) {
+          needsToBeAdded = false;
+          newSet.push([min, actualMax]);
+        }
+
+        // min is within range but max is out - extend max
+        if (min >= actualMin && min <= actualMax && max >= actualMax) {
+          needsToBeAdded = false;
+          newSet.push([actualMin, max]);
+        }
+
+        // range is a superset of another
+        if (min <= actualMin && max >= actualMax) {
+          needsToBeAdded = false;
+          newSet.push([min, max]);
+        }
+
+        // range is before or after
+        if (max < actualMin || min > actualMax) {
+          newSet.push([actualMin, actualMax]);
+        }
+      }
+
+      if (needsToBeAdded) {
+        newSet.push([min, max]);
+      }
+
+      return newSet;
+    }, [])
+    .sort((a, b) => (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
 }
 
 run(__filename, solve);
