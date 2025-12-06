933875db5e0cf3671e489c02b97cd3d8b986d5bd
Tim Trinidad <timtrinidad@gmail.com>
2025-12-06 01:33:17 -0800

    feat(2025-06): add js challenge part 2 - 0.001s

--- js/2025/aoc2506.js
+++ js/2025/aoc2506.js
@@ -8,12 +8,14 @@ async function solve(inputs, partNum, isSample) {
 }
 
 function parse(inputs) {
-  const parsed = inputs.map((x) => x.trim().split(/\s+/));
-  const ops = parsed.pop();
-  return { nums: parsed.map((x) => x.map((y) => parseInt(y, 10))), ops };
+  return inputs;
 }
 
-function part1({ nums, ops }) {
+function part1(parsed) {
+  const split = parsed.map((x) => x.trim().split(/\s+/));
+  const ops = split.pop();
+  const nums = split.map((x) => x.map((y) => parseInt(y, 10)));
+
   const totals = ops.map((op, x) =>
     nums
       .map((row) => row[x])
@@ -23,7 +25,42 @@ function part1({ nums, ops }) {
 }
 
 function part2(parsed) {
-  return 0;
+  const nums = parsed.map((x) => x.split(""));
+  const ops = nums.pop();
+
+  let groupOps = [];
+  let groupNums = [];
+  let groupNum = 0;
+  // Go char column by column until we find a col with all spaces
+  for (let x = 0; x < ops.length; x++) {
+    // Capture the op for this group
+    if (ops[x] !== " ") {
+      groupOps[groupNum] = ops[x];
+    }
+    // Gather all characters in this column
+    const num = nums.map((row) => row[x]);
+
+    // If all blanks, we've finished the column. Move onto next group.
+    if (num.every((char) => char === " ")) {
+      groupNum++;
+      continue;
+    }
+
+    // Parse the numbers in the column and add to current group
+    numParsed = parseInt(num.filter((char) => char !== " ").join(""), 10);
+    if (!groupNums[groupNum]) {
+      groupNums[groupNum] = [];
+    }
+    groupNums[groupNum].push(numParsed);
+  }
+
+  return groupOps
+    .map((op, groupNum) =>
+      groupNums[groupNum].reduce((acc, curr) =>
+        op === "*" ? acc * curr : acc + curr
+      )
+    )
+    .reduce((acc, curr) => acc + curr);
 }
 
 run(__filename, solve);
